28486680ce09f7fe12a290000cb19abff699059a
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2020-10-02 11:46:37 -0500

    Some restrictions were added
    The / force and / kill commands cannot be used by a player who has captured the flag.
    
    The /drop command can only be used by the player who captured the flag.

--- CaptureTheFlag/CaptureTheFlag/Command/CmdPublic.cs
+++ CaptureTheFlag/CaptureTheFlag/Command/CmdPublic.cs
@@ -11,17 +11,42 @@ namespace CaptureTheFlag.Command
     [CommandGroup("public")]
     public class CmdPublic
     {
+        private static bool IsCaptureFlag(Player player)
+        {
+            if(player.IsCapturedFlag())
+            {
+                player.SendClientMessage(Color.Red, "Error: Usted no puede usar este comando.");
+                return true;
+            }
+            return false;
+        }
+
         [Command("force", Shortcut = "force")]
         private static void Force(Player player)
         {
+            if (IsCaptureFlag(player)) return;
             player.SetForceClass();
         }
 
         [Command("kill", Shortcut = "kill")]
         private static void Kill(Player player)
         {
+            if (IsCaptureFlag(player)) return;
             player.Health = 0;
             ++player.Data.Deaths;
+        }
+
+        [Command("drop", Shortcut = "drop")]
+        private static void Drop(Player player)
+        {
+            if (!player.IsCapturedFlag())
+                player.SendClientMessage(Color.Red, "Error: Usted no ha capturado la bandera.");
+            else
+            {
+                player.PlayerTeam.TeamRival.Drop(player);
+                player.Position = new Vector3(player.Position.X + 2.0f, player.Position.Y, player.Position.Z);
+                player.RemoveAttachedObject(0);
+            }
         }
 
         [Command("team", Shortcut = "team")]
