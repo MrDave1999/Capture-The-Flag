5c139ffa79d040f201abdb0b039d630bae899974
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2020-10-03 12:02:50 -0500

    The / drop command has been removed for abuse reasons

--- CaptureTheFlag/CaptureTheFlag/Command/CmdPublic.cs
+++ CaptureTheFlag/CaptureTheFlag/Command/CmdPublic.cs
@@ -36,19 +36,6 @@ namespace CaptureTheFlag.Command
             ++player.Data.Deaths;
         }
 
-        [Command("drop", Shortcut = "drop")]
-        private static void Drop(Player player)
-        {
-            if (!player.IsCapturedFlag())
-                player.SendClientMessage(Color.Red, "Error: Usted no ha capturado la bandera.");
-            else
-            {
-                player.PlayerTeam.TeamRival.Drop(player);
-                player.Position = new Vector3(player.Position.X + 2.0f, player.Position.Y, player.Position.Z);
-                player.RemoveAttachedObject(0);
-            }
-        }
-
         [Command("team", Shortcut = "team")]
         private static void Team(Player player)
         {
