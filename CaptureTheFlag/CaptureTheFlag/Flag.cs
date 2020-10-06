8024d4536649db738f80b987702216b6ae7d6518
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2020-10-06 11:38:37 -0500

    CreateAll and RemoveAll method has been added

--- CaptureTheFlag/CaptureTheFlag/Flag.cs
+++ CaptureTheFlag/CaptureTheFlag/Flag.cs
@@ -2,15 +2,17 @@
 using SampSharp.GameMode.Definitions;
 using SampSharp.GameMode.SAMP;
 using SampSharp.GameMode.World;
+using SampSharp.Streamer.World;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Text;
 
 namespace CaptureTheFlag
 {
     public class Flag
     {
-        public Player PlayerCaptured { get; set; } //el jugador que ha capturado la bandera.
+        public Player PlayerCaptured { get; set; } 
         public bool IsPositionBase { get; set; } = true; 
         public Color ColorHex { get; set; }
         public int Model { get; set; }
@@ -29,14 +31,32 @@ namespace CaptureTheFlag
             player.SetAttachedObject(0, Model, Bone.Spine, new Vector3(-0.099000, -0.156000, -0.194000), new Vector3(8.399998, 41.500011, 17.199983), new Vector3(0.486000, 1.295001, 0.571000), ColorHex, ColorHex);
         }
 
+        public void Create(Vector3 vector3)
+        {
+            Pickup.Create(Model, 1, vector3);
+        }
+
         public void Create()
         {
             Pickup.Create(Model, 1, PositionBase);
+            
         }
 
         public void Create(Player player)
         {
             Pickup.Create(Model, 1, player.Position);
+        }
+
+        public static void RemoveAll()
+        {
+            foreach(Pickup pickup in Pickup.GetAll<Pickup>())
+                pickup.Dispose();
+        }
+
+        public static void CreateAll(Vector3 vector3)
+        {
+            GameMode.TeamAlpha.Flag.Create(vector3);
+            GameMode.TeamBeta.Flag.Create(vector3);
         }
     }
 
