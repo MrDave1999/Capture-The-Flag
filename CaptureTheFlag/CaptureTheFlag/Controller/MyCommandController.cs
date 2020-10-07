0caac3d0dbd3143279bf3d1a8875d152135c1d9c
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2020-10-07 13:42:40 -0500

    Create MyCommandController.cs

---
+++ CaptureTheFlag/CaptureTheFlag/Controller/MyCommandController.cs
@@ -0,0 +1,35 @@
+﻿using SampSharp.GameMode;
+using SampSharp.GameMode.Controllers;
+using SampSharp.GameMode.SAMP;
+using SampSharp.GameMode.SAMP.Commands;
+using SampSharp.GameMode.SAMP.Commands.PermissionCheckers;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace CaptureTheFlag.Controller
+{
+    [Controller]
+    public class MyCommandController : CommandController
+    {
+        public override void RegisterServices(BaseMode gameMode, GameModeServiceContainer serviceContainer)
+        {
+            CommandsManager = new MyCommandsManager(gameMode);
+            serviceContainer.AddService(CommandsManager);
+            CommandsManager.RegisterCommands(gameMode.GetType());
+        }
+    }
+
+    public class MyCommandsManager : CommandsManager
+    {
+        public MyCommandsManager(BaseMode gameMode) : base(gameMode)
+        {
+        }
+
+        protected override ICommand CreateCommand(CommandPath[] commandPaths, string displayName, bool ignoreCase, IPermissionChecker[] permissionCheckers, MethodInfo method, string usageMessage)
+        {
+            return new DefaultCommand(commandPaths, displayName, ignoreCase, permissionCheckers, method, $"{Color.Red}Uso: {usageMessage}");
+        }
+    }
+}
