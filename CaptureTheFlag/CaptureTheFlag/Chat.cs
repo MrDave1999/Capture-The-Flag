5c181cc4bea8a269ce9ae8890e87c36d4211ef5d
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2020-10-07 13:41:04 -0500

    Chat class is in charge of writing the text in the chat

---
+++ CaptureTheFlag/CaptureTheFlag/Chat.cs
@@ -0,0 +1,48 @@
+﻿using CaptureTheFlag.Command;
+using SampSharp.GameMode.SAMP;
+using SampSharp.GameMode.World;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace CaptureTheFlag
+{
+    public class Chat
+    {
+        public static void AlterString(string text, char c)
+        {
+            unsafe
+            {
+                fixed (char* str = text)
+                    str[0] = c;
+            }
+        }
+
+        public static void WriteText(Player player, string text)
+        {
+            char symbol = text[0];
+            AlterString(text, ' ');
+            switch (symbol)
+            {
+                case '!': //TeamChat
+                    CmdPublic.TeamChat(player, text);
+                    break;
+                case '$': //VipChat
+                    break;
+                case '#': //AdminChat
+                    break;
+                default:
+                    AlterString(text, symbol);
+                    if(player.Data.LevelAdmin >= 1)
+                        BasePlayer.SendClientMessageToAll($"{player.Color}{player.Name} {{00FF00}}[{player.Id}]: {Color.White}{text}");
+                    else if(player.Data.LevelVip >= 1)
+                        BasePlayer.SendClientMessageToAll($"{player.Color}{player.Name} {Color.Yellow}[{player.Id}]: {Color.White}{text}");
+                    else
+                        BasePlayer.SendClientMessageToAll($"{player.Color}{player.Name} {Color.White}[{player.Id}]: {text}");
+                    break;
+            }
+        }
+
+    }
+}
