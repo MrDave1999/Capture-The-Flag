ba500f069ad42ca98268769e3dc76ff43f0965e1
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2020-10-08 13:54:44 -0500

    Informational messages updated

--- CaptureTheFlag/CaptureTheFlag/Team.cs
+++ CaptureTheFlag/CaptureTheFlag/Team.cs
@@ -11,18 +11,20 @@ namespace CaptureTheFlag
 {
     public class Team
     {
-        public TeamID Id { get; set; }
+        public TeamID Id { get; private set; }
         public int Members { get; set; }
         public int Score { get; set; }
-        public int Skin { get; set; }
-        public string NameTeam { get; set; }
-        public string NameColor { get; set; }
-        public string ColorGameText { get; set; }
-        public string OtherColor { get; set; }
-        public Flag Flag { get; set; }
+        public int Kills { get; set; }
+        public int Deaths { get; set; }
+        public int Skin { get; private set; }
+        public string NameTeam { get; private set; }
+        public string NameColor { get; private set; }
+        public string ColorGameText { get; private set; }
+        public string OtherColor { get; private set; }
+        public Flag Flag { get; private set; }
         public Team TeamRival { get; set; }
-        public TextDraw TdScore { get; set; }
-        public DynamicMapIcon Icon { get; set; }
+        public TextDraw TdScore { get; private set; }
+        public DynamicMapIcon Icon { get; private set; }
 
         public Team(int skin, string otherColor, string colorGameText, TextDraw tdscore, TeamID teamid, string name, string namecolor, Flag flag, Color colorIcon)
         {
@@ -86,23 +88,23 @@ namespace CaptureTheFlag
             Flag.PlayerCaptured = player;
             if (takeInPosBase)
             {
-                BasePlayer.SendClientMessageToAll(Color.Yellow, $"[CTF]: {player.Name} ha capturado la bandera {NameColor} del equipo {NameTeam}.");
-                player.SendClientMessage(Color.LimeGreen, "[CTF]: Capturaste la bandera, debes llevarla a tu base!");
-                player.SendClientMessage(Color.LimeGreen, "[CTF]: Obtuviste +6 de score por capturar la bandera.");
+                BasePlayer.SendClientMessageToAll($"{OtherColor}[Team {NameTeam}]: {player.Name} capturó la bandera {NameColor} del equipo {NameTeam}.");
+                player.SendClientMessage($"{Color.Pink}[!]: {Color.White}Capturaste la bandera, debes llevarla a tu base.");
+                player.SendClientMessage($"{Color.Pink}[!]: {Color.White}Obtuviste +6 de score por capturar la bandera.");
                 player.Score += 6;
             }
             else
             {
-                BasePlayer.SendClientMessageToAll(Color.Yellow, $"[CTF]: {player.Name} ha tomado la bandera {NameColor} del equipo {NameTeam}.");
-                player.SendClientMessage(Color.LimeGreen, "[CTF]: Debes llevar esa bandera a tu base, apura!");
+                BasePlayer.SendClientMessageToAll($"{OtherColor}[Team {NameTeam}]: {player.Name} tomó la bandera {NameColor} del equipo {NameTeam}.");
+                player.SendClientMessage($"{Color.Pink}[!]: {Color.White}Debes llevar esa bandera a tu base.");
             }
         }
 
         public void Carry(Player player)
         {
-            BasePlayer.SendClientMessageToAll(Color.Yellow, $"[CTF]: {player.Name} ha llevado la bandera {NameColor} del equipo {NameTeam} a su base.");
-            player.SendClientMessage(Color.LimeGreen, "[CTF]: Obtuviste +10 de score por llevar la bandera a tu base.");
-            BasePlayer.GameTextForAll($"~>~{TeamRival.ColorGameText}+1 score team {TeamRival.NameTeam}!", 5000, 5);
+            BasePlayer.SendClientMessageToAll($"{OtherColor}[Team {NameTeam}]: {player.Name} llevó la bandera {NameColor} del equipo {NameTeam} a su base.");
+            player.SendClientMessage($"{Color.Pink}[!]: {Color.White}Obtuviste +10 de score por llevar la bandera a tu base.");
+            BasePlayer.GameTextForAll($"~>~{TeamRival.ColorGameText}+1 score team {TeamRival.NameTeam}", 5000, 3);
             player.RemoveAttachedObject(0);
             Flag.Create(); 
             Flag.PlayerCaptured = null;
@@ -116,8 +118,8 @@ namespace CaptureTheFlag
         {
             Flag.IsPositionBase = true;
             Flag.Create();
-            BasePlayer.SendClientMessageToAll(Color.Yellow, $"[CTF]: {player.Name} ha recuperado la bandera {NameColor} del equipo {NameTeam}.");
-            player.SendClientMessage(Color.LimeGreen, "[CTF]: Obtuviste +4 de score por recuperar la bandera.");
+            BasePlayer.SendClientMessageToAll($"{OtherColor}[Team {NameTeam}]: {player.Name} recuperó la bandera {NameColor} del equipo {NameTeam}.");
+            player.SendClientMessage($"{Color.Pink}[!]: {Color.White}Obtuviste +4 de score por recuperar la bandera.");
             player.Score += 4;
         }
 
@@ -126,7 +128,7 @@ namespace CaptureTheFlag
             Drop(player);
             if (killer != null)
             {
-                killer.SendClientMessage(Color.LimeGreen, "[CTF]: Obtuviste +4 de score por matar al portador.");
+                killer.SendClientMessage($"{Color.Pink}[!]: {Color.White}Obtuviste +4 de score por matar al portador.");
                 killer.Score += 4;
             }
         }
@@ -136,15 +138,15 @@ namespace CaptureTheFlag
             Flag.Create(player);
             Flag.PlayerCaptured = null;
             Flag.IsPositionBase = false;
-            BasePlayer.SendClientMessageToAll(Color.Yellow, $"[CTF]: La bandera {NameColor} del equipo {NameTeam} fue soltada por {player.Name}.");
+            BasePlayer.SendClientMessageToAll($"{OtherColor}[Team {NameTeam}]: {player.Name} dejó caer la bandera {NameColor} del equipo {NameTeam}.");
         }
     }
 
     public enum TeamID
     {
-       None,
        Alpha,
-       Beta
+       Beta,
+       None
     }
 
     public class SkinTeam
