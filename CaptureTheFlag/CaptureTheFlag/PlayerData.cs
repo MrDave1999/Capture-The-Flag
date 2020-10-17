a4c5cdd74fefd0a9141f1d2e3182e9d246c1bd54
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2020-10-17 14:02:09 -0500

    New fields added

--- CaptureTheFlag/CaptureTheFlag/PlayerData.cs
+++ CaptureTheFlag/CaptureTheFlag/PlayerData.cs
@@ -6,9 +6,12 @@ namespace CaptureTheFlag
 {
     public class PlayerData
     {
+        public string password { get; set; }
         public int Kills { get; set; }
         public int Deaths { get; set; }
         public int LevelAdmin { get; set; }
         public int LevelVip { get; set; }
+        public int LevelGame { get; set; }
+        public int DroppedFlags { get; set; }
     }
 }
