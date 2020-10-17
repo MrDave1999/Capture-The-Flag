935bbed7f62ec48f753c03686c808e4ef40141a6
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2020-10-17 14:01:31 -0500

    BlockCommand class added
    This class blocks commands if the player is in class selection.

---
+++ CaptureTheFlag/CaptureTheFlag/BlockCommand.cs
@@ -0,0 +1,22 @@
+﻿using SampSharp.GameMode.SAMP;
+using SampSharp.GameMode.SAMP.Commands.PermissionCheckers;
+using SampSharp.GameMode.World;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaptureTheFlag
+{
+    public class BlockCommand : IPermissionChecker
+    {
+        public string Message
+        {
+            get { return $"{Color.Red}No puedes usar comandos en la selección de clases."; }
+        }
+
+        public bool Check(BasePlayer player)
+        {
+            return !((Player)player).IsSelectionClass;
+        }
+    }
+}
