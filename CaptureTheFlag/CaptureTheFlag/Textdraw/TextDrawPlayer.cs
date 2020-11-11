dfe312cd7deb71c119399ad5f3fb8f55eb52f9d6
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2020-11-11 13:26:29 -0500

    Added a player info bar like textdraw

--- CaptureTheFlag/CaptureTheFlag/Textdraw/TextDrawPlayer.cs
+++ CaptureTheFlag/CaptureTheFlag/Textdraw/TextDrawPlayer.cs
@@ -1,11 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Text;
+using SampSharp.GameMode;
+using SampSharp.GameMode.Definitions;
+using SampSharp.GameMode.Display;
 
 namespace CaptureTheFlag.Textdraw
 {
-    public class TextDrawPlayer
+    public static class TextDrawPlayer
     {
-        //code.
+        public static void CreateTDStats(PlayerTextDraw stats)
+        {
+            stats.Position = new Vector2(638.000000, 433.000000);
+            stats.Text = "~g~~h~] ~w~KILLS: ~y~0 ~g~~h~- ~w~DEATHS: ~y~0 ~g~~h~- ~w~LVL: ~y~0 ~g~~h~- ~w~ADRENALINE: ~y~0/100~g~~h~ ]";
+            stats.Font = TextDrawFont.Slim;
+            stats.LetterSize = new Vector2(0.279166, 1.350000);
+            stats.Outline = 1;
+            stats.Alignment = TextDrawAlignment.Right;
+            stats.ForeColor = -1;
+            stats.BackColor= 255;
+            stats.Proportional = true;
+        }
+
+        public static void UpdateTdStats(Player player)
+        {
+            player.Stats.Text = $"~g~~h~] ~w~KILLS: ~y~{player.Kills} ~g~~h~- ~w~DEATHS: ~y~{player.Deaths} ~g~~h~- ~w~LVL: ~y~{player.Data.LevelGame} ~g~~h~- ~w~ADRENALINE: ~y~{player.Adrenaline}/100~g~~h~ ]";
+            player.Stats.Show();
+        }
+
+        public static void Destroy(Player player)
+        {
+            player.Stats.Dispose();
+        }
+
+        public static void Show(Player player)
+        {
+            player.Stats.Show();
+        }
+
+        public static void Hide(Player player)
+        {
+            player.Stats.Hide();
+        }
+
     }
 }
