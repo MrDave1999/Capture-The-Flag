e23501c6e9b8749b1fd45ff9e3f931b235cc907a
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2020-11-11 13:26:47 -0500

    Update stats user

--- CaptureTheFlag/CaptureTheFlag/PlayerData.cs
+++ CaptureTheFlag/CaptureTheFlag/PlayerData.cs
@@ -7,8 +7,8 @@ namespace CaptureTheFlag
     public class PlayerData
     {
         public string password { get; set; }
-        public int Kills { get; set; }
-        public int Deaths { get; set; }
+        public int TotalKills { get; set; }
+        public int TotalDeaths { get; set; }
         public int LevelAdmin { get; set; }
         public int LevelVip { get; set; }
         public int LevelGame { get; set; }
