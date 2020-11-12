9a2392829620683ba08986730b7062ef66aad6c4
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2020-11-12 11:17:31 -0500

    Added a textdraw where the health and armour bar is displayed

--- CaptureTheFlag/CaptureTheFlag/Textdraw/TextDrawPlayer.cs
+++ CaptureTheFlag/CaptureTheFlag/Textdraw/TextDrawPlayer.cs
@@ -23,6 +23,32 @@ namespace CaptureTheFlag.Textdraw
             stats.Proportional = true;
         }
 
+        public static void CreateTDHealth(PlayerTextDraw health)
+        {
+            health.Position = new Vector2(578.000000, 66.000000);
+            health.Text = "100";
+            health.Font = TextDrawFont.Slim;
+            health.LetterSize = new Vector2(0.241666, 0.899999);
+            health.Outline = 1;
+            health.Alignment = TextDrawAlignment.Center;
+            health.ForeColor = -1;
+            health.BackColor = 255;
+            health.Proportional = true;
+        }
+
+        public static void CreateTDArmour(PlayerTextDraw armour)
+        {
+            armour.Position = new Vector2(578.000000, 44.000000);
+            armour.Text = "0";
+            armour.Font = TextDrawFont.Slim;
+            armour.LetterSize = new Vector2(0.241666, 0.899999);
+            armour.Outline = 1;
+            armour.Alignment = TextDrawAlignment.Center;
+            armour.ForeColor = -1;
+            armour.BackColor = 255;
+            armour.Proportional = true;
+        }
+
         public static void UpdateTdStats(Player player)
         {
             player.Stats.Text = $"~g~~h~] ~w~KILLS: ~y~{player.Kills} ~g~~h~- ~w~DEATHS: ~y~{player.Deaths} ~g~~h~- ~w~LVL: ~y~{player.Data.LevelGame} ~g~~h~- ~w~ADRENALINE: ~y~{player.Adrenaline}/100~g~~h~ ]";
@@ -32,16 +58,21 @@ namespace CaptureTheFlag.Textdraw
         public static void Destroy(Player player)
         {
             player.Stats.Dispose();
+            player.THealth.Dispose();
+            player.TArmour.Dispose();
         }
 
         public static void Show(Player player)
         {
             player.Stats.Show();
+            player.THealth.Show();
         }
 
         public static void Hide(Player player)
         {
             player.Stats.Hide();
+            player.THealth.Hide();
+            player.TArmour.Hide();
         }
 
     }
