d09b6fb97ab285231daa326eb8b6834eda88c947
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2020-11-12 13:12:42 -0500

    Killing sprees was added to the player information bar

--- CaptureTheFlag/CaptureTheFlag/Textdraw/TextDrawPlayer.cs
+++ CaptureTheFlag/CaptureTheFlag/Textdraw/TextDrawPlayer.cs
@@ -13,7 +13,7 @@ namespace CaptureTheFlag.Textdraw
         public static void CreateTDStats(PlayerTextDraw stats)
         {
             stats.Position = new Vector2(638.000000, 433.000000);
-            stats.Text = "~g~~h~] ~w~KILLS: ~y~0 ~g~~h~- ~w~DEATHS: ~y~0 ~g~~h~- ~w~LVL: ~y~0 ~g~~h~- ~w~ADRENALINE: ~y~0/100~g~~h~ ]";
+            stats.Text = "~g~~h~] ~w~KILLS: ~y~0 ~g~~h~- ~w~DEATHS: ~y~0 ~g~~h~- ~w~LVL: ~y~0 ~g~~h~- ~w~KS: ~y~0 ~g~~h~- ~w~ADRENALINE: ~y~0/100~g~~h~ ]";
             stats.Font = TextDrawFont.Slim;
             stats.LetterSize = new Vector2(0.279166, 1.350000);
             stats.Outline = 1;
@@ -51,7 +51,7 @@ namespace CaptureTheFlag.Textdraw
 
         public static void UpdateTdStats(Player player)
         {
-            player.Stats.Text = $"~g~~h~] ~w~KILLS: ~y~{player.Kills} ~g~~h~- ~w~DEATHS: ~y~{player.Deaths} ~g~~h~- ~w~LVL: ~y~{player.Data.LevelGame} ~g~~h~- ~w~ADRENALINE: ~y~{player.Adrenaline}/100~g~~h~ ]";
+            player.Stats.Text = $"~g~~h~] ~w~KILLS: ~y~{player.Kills} ~g~~h~- ~w~DEATHS: ~y~{player.Deaths} ~g~~h~- ~w~LVL: ~y~{player.Data.LevelGame} ~g~~h~- ~w~KS: ~y~{player.Data.KillingSprees} ~g~~h~- ~w~ADRENALINE: ~y~{player.Adrenaline}/100~g~~h~ ]";
             player.Stats.Show();
         }
 
