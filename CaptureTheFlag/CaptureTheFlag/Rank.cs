12f313c52b6717c84128c40d7d961a9e1a51a83b
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2020-11-13 13:01:09 -0500

    New ranges were added

--- CaptureTheFlag/CaptureTheFlag/Rank.cs
+++ CaptureTheFlag/CaptureTheFlag/Rank.cs
@@ -6,6 +6,35 @@ namespace CaptureTheFlag
 {
     public class Rank
     {
+        public static readonly int MAX_RANK = 15;
+        private string nameRank;
+        private int kills;
+
+        private Rank(string nameRank, int kills)
+        {
+            this.nameRank = nameRank;
+            this.kills = kills;
+        }
+
+        private static Rank[] rankGame = new[]
+        {
+            new Rank("Noob",            0),
+            new Rank("Medium",          50),
+            new Rank("Junior",          100),
+            new Rank("Semi-Advanced",   200),
+            new Rank("Advanced",        300),
+            new Rank("Hitman",          400),
+            new Rank("Extreme",         500),
+            new Rank("Aniquilador",     600),
+            new Rank("Maniático",       700),
+            new Rank("Invencible",      800),
+            new Rank("Senior",          900),
+            new Rank("GameMaster",      1000),
+            new Rank("Pro",             2000),
+            new Rank("SuperPro",        3000),
+            new Rank("Legendary",       4000)
+        };
+
         private static string[] rankVip = new[]
         {
             "Ninguno",
@@ -32,6 +61,16 @@ namespace CaptureTheFlag
         public static string GetRankAdmin(int index)
         {
             return rankAdmin[index];
+        }
+
+        public static string GetRankLevel(int level)
+        {
+            return rankGame[level - 1].nameRank;
+        }
+
+        public static int GetRequiredKills(int level)
+        {
+            return rankGame[level - 1].kills;
         }
     }
 }
