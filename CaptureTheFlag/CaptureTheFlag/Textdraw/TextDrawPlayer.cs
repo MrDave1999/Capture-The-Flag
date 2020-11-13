b6a479e0c38d32333293428599292cdd2ab05699
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2020-11-13 11:56:36 -0500

    Update TextDrawPlayer.cs

--- CaptureTheFlag/CaptureTheFlag/Textdraw/TextDrawPlayer.cs
+++ CaptureTheFlag/CaptureTheFlag/Textdraw/TextDrawPlayer.cs
@@ -13,7 +13,7 @@ namespace CaptureTheFlag.Textdraw
         public static void CreateTDStats(PlayerTextDraw stats)
         {
             stats.Position = new Vector2(638.000000, 433.000000);
-            stats.Text = "~g~~h~] ~w~KILLS: ~y~0 ~g~~h~- ~w~DEATHS: ~y~0 ~g~~h~- ~w~LVL: ~y~0 ~g~~h~- ~w~KS: ~y~0 ~g~~h~- ~w~ADRENALINE: ~y~0/100~g~~h~ ]";
+            stats.Text = "";
             stats.Font = TextDrawFont.Slim;
             stats.LetterSize = new Vector2(0.279166, 1.350000);
             stats.Outline = 1;
