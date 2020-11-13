c25bc7d4f51b6dc90fc2d38c1fe73c1f887fbf42
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2020-11-13 16:11:30 -0500

    Update ranks

--- CaptureTheFlag/CaptureTheFlag/Rank.cs
+++ CaptureTheFlag/CaptureTheFlag/Rank.cs
@@ -21,18 +21,18 @@ namespace CaptureTheFlag
             new Rank("Noob",            0),
             new Rank("Medium",          50),
             new Rank("Junior",          100),
-            new Rank("Semi-Advanced",   200),
-            new Rank("Advanced",        300),
-            new Rank("Hitman",          400),
-            new Rank("Extreme",         500),
-            new Rank("Aniquilador",     600),
-            new Rank("Maniático",       700),
-            new Rank("Invencible",      800),
-            new Rank("Senior",          900),
-            new Rank("GameMaster",      1000),
-            new Rank("Pro",             2000),
-            new Rank("SuperPro",        3000),
-            new Rank("Legendary",       4000)
+            new Rank("Semi-Advanced",   150),
+            new Rank("Advanced",        200),
+            new Rank("Hitman",          250),
+            new Rank("Extreme",         300),
+            new Rank("Annihilator",     350),
+            new Rank("Maniac",          400),
+            new Rank("Invincible",      450),
+            new Rank("Senior",          500),
+            new Rank("GameMaster",      550),
+            new Rank("Professional",    600),
+            new Rank("SuperPro",        650),
+            new Rank("Legendary",       700)
         };
 
         private static string[] rankVip = new[]
