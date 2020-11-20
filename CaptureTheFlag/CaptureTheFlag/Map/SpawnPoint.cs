d92e7c12da3dbd216f52baa69b629dfe54232ec1
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2020-11-20 14:16:40 -0500

    Create SpawnPoint.cs

---
+++ CaptureTheFlag/CaptureTheFlag/Map/SpawnPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaptureTheFlag.Map
+{
+    public class SpawnPoint
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        public float Angle { get; set; }
+
+        public SpawnPoint() { }
+
+    }
+}
