cbaf8e0c08034eb38522e6c9bcbdf01c3db576ac
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2020-11-20 14:18:14 -0500

    The command manager was updated

--- CaptureTheFlag/CaptureTheFlag/Controller/MyCommandController.cs
+++ CaptureTheFlag/CaptureTheFlag/Controller/MyCommandController.cs
@@ -1,8 +1,11 @@
-﻿using SampSharp.GameMode;
+﻿using CaptureTheFlag.Map;
+using SampSharp.GameMode;
 using SampSharp.GameMode.Controllers;
 using SampSharp.GameMode.SAMP;
 using SampSharp.GameMode.SAMP.Commands;
+using SampSharp.GameMode.SAMP.Commands.ParameterTypes;
 using SampSharp.GameMode.SAMP.Commands.PermissionCheckers;
+using SampSharp.GameMode.World;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
@@ -29,7 +32,27 @@ namespace CaptureTheFlag.Controller
 
         protected override ICommand CreateCommand(CommandPath[] commandPaths, string displayName, bool ignoreCase, IPermissionChecker[] permissionCheckers, MethodInfo method, string usageMessage)
         {
-            return new DefaultCommand(commandPaths, displayName, ignoreCase, permissionCheckers, method, $"{Color.Red}Uso: {usageMessage}");
+            return new MyCommand(commandPaths, displayName, ignoreCase, permissionCheckers, method, $"{Color.Red}Uso: {usageMessage}");
         }
     }
+
+    public class MyCommand : DefaultCommand
+    {
+        public MyCommand(CommandPath[] names, string displayName, bool ignoreCase,
+            IPermissionChecker[] permissionCheckers, MethodInfo method, string usageMessage)
+            : base(names, displayName, ignoreCase, permissionCheckers, method, usageMessage)
+        {
+        }
+
+        protected override bool SendPermissionDeniedMessage(IPermissionChecker permissionChecker, BasePlayer sender)
+        {
+            var player = sender as Player;
+            if (player.IsSelectionClass)
+                player.SendClientMessage(Color.Red, "Error: No puedes usar comandos en la selección de clases.");
+            else if (CurrentMap.IsLoading)
+                player.SendClientMessage(Color.Red, "Error: No puedes usar comandos mientras el mapa se está cargando.");
+            return true;
+        }
+    }
+
 }
