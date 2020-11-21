f9194dd142ba69daece8989085c0507bf9238aca
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2020-11-21 14:42:44 -0500

    Create ParseData.cs

---
+++ CaptureTheFlag/CaptureTheFlag/ParseData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace CaptureTheFlag
+{
+    public static class ParseData
+    {
+        public static double Double(string s)
+        {
+            return double.Parse(s, CultureInfo.InvariantCulture);
+        }
+
+        public static float Float(string s)
+        {
+            return float.Parse(s, CultureInfo.InvariantCulture);
+        }
+    }
+}
