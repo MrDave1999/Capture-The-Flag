e3b887543a8d3d9dbf9a123a8525d4fb1077b88e
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2020-11-30 11:05:53 -0500

    Rand class was added to handle random integer numbers

---
+++ CaptureTheFlag/CaptureTheFlag/Rand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaptureTheFlag
+{
+    public static class Rand
+    {
+        private static readonly Random random = new Random();
+
+        public static int Next(int max)
+        {
+            return random.Next(max);
+        }
+
+    }
+}
