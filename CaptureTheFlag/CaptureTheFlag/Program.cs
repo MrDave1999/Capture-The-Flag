0fb0c7a79f52e777c7fff41e9829f8bea1aade5b
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2020-12-21 19:23:19 -0500

    Update Program.cs

--- CaptureTheFlag/CaptureTheFlag/Program.cs
+++ CaptureTheFlag/CaptureTheFlag/Program.cs
@@ -9,6 +9,7 @@ namespace CaptureTheFlag
         public static void Main(string[] args)
         {
             new GameModeBuilder()
+                .RedirectConsoleOutput()
                 .UseEncoding(Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "codepages"+ Path.DirectorySeparatorChar + "cp1252.txt"))
                 .Use<GameMode>()
                 .Run();
