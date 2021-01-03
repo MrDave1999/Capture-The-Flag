f5438000168a8c38040b937d201186bcd10d5b8c
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2021-01-03 09:36:46 -0500

    This class is in charge of making the connection to the database

---
+++ CaptureTheFlag/CaptureTheFlag/DataBase/DBConnect.cs
@@ -0,0 +1,39 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaptureTheFlag.DataBase
+{
+    public static class DBConnect
+    {
+        public static MySqlConnection Connection { get; set; }
+
+        public static void Open()
+        {
+            string cs = "server=localhost;port=3306;username=root;password=1234;database=gamemode;";
+            try
+            {
+                Connection = new MySqlConnection(cs);
+                Connection.Open();
+                Console.WriteLine("  The database connection was successful!");
+            }
+            catch(MySqlException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+
+        public static void Close()
+        {
+            try
+            {
+                Connection.Close();
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
