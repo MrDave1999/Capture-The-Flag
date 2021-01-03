b3c73728cc27226fcf1a4840f62823fa5ae884c0
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2021-01-03 09:37:06 -0500

    Some validation methods

---
+++ CaptureTheFlag/CaptureTheFlag/DataBase/Validate.cs
@@ -0,0 +1,30 @@
+﻿using SampSharp.GameMode.Display;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaptureTheFlag.DataBase
+{
+    public class Validate
+    {
+        public static void PasswordRange(Player player, InputDialog dialog, string pass)
+        {
+            if(pass.Length < 4 || pass.Length > 20)
+            {
+                dialog.Message = "La contraseña debe estar entre 4 a 20 caracteres.\nIngrese una contraseña:";
+                dialog.Show(player);
+                throw new Exception();
+            }
+        }
+
+        public static void IsEmpty(Player player, InputDialog dialog, string text)
+        {
+            if(text.Trim().Length == 0)
+            {
+                dialog.Message = "No puedes dejar el campo vacío.\nIngrese una contraseña:";
+                dialog.Show(player);
+                throw new Exception();
+            }
+        } 
+    }
+}
