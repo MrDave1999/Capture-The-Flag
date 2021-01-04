a3e66416943657dac99c03b4813cfdd8c4173d21
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2021-01-03 20:34:32 -0500

    Update Validate.cs

--- CaptureTheFlag/CaptureTheFlag/DataBase/Validate.cs
+++ CaptureTheFlag/CaptureTheFlag/DataBase/Validate.cs
@@ -1,5 +1,6 @@
 ﻿using CaptureTheFlag.PropertiesPlayer;
 using SampSharp.GameMode.Display;
+using SampSharp.GameMode.SAMP;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,8 +13,17 @@ namespace CaptureTheFlag.DataBase
         {
             if(pass.Length < 4 || pass.Length > 20)
             {
-                dialog.Message = "La contraseña debe estar entre 4 a 20 caracteres.\nIngrese una contraseña:";
+                dialog.Message = "La contraseña debe tener entre 4 y 20 caracteres.\nIngrese una contraseña:";
                 dialog.Show(player);
+                throw new Exception();
+            }
+        }
+
+        public static void PasswordRange(Player player, string password)
+        {
+            if (password.Length < 4 || password.Length > 20)
+            {
+                player.SendClientMessage(Color.Red, "Error: La contraseña debe tener entre 4 y 20 caracteres.");   
                 throw new Exception();
             }
         }
