24b2f0d67f34e3b150fab782f8dd8a994dd4adbf
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2021-01-22 18:30:38 -0500

    Update FileRead.cs

--- CaptureTheFlag/CaptureTheFlag/Map/FileRead.cs
+++ CaptureTheFlag/CaptureTheFlag/Map/FileRead.cs
@@ -19,7 +19,14 @@ namespace CaptureTheFlag.Map
 
         static FileRead()
         {
-            iniFile = File.ReadAllText(GetPath("flag_position.ini"));
+            try
+            {
+                iniFile = File.ReadAllText(GetPath("flag_position.ini"));
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine($"Error {e.StackTrace} Reason: {e.Message}");
+            }
         }
 
         public static string GetPath(string part)
@@ -33,20 +40,34 @@ namespace CaptureTheFlag.Map
 
         public static void ConfigMapRead()
         {
-            var data = new IniDataParser().Parse(File.ReadAllText(GetPath("config_maps.ini")));
-            MAX_MAPS = int.Parse(data["Map"]["MAX_MAPS"]);
-            MAX_TIME_ROUND = int.Parse(data["Map"]["MAX_TIME_ROUND"]);
-            MAX_SPAWNS = int.Parse(data["Map"]["MAX_SPAWNS"]);
-            MAX_TIME_LOADING = int.Parse(data["Map"]["MAX_TIME_LOADING"]);
+            try
+            {
+                var data = new IniDataParser().Parse(File.ReadAllText(GetPath("config_maps.ini")));
+                MAX_MAPS = int.Parse(data["Map"]["MAX_MAPS"]);
+                MAX_TIME_ROUND = int.Parse(data["Map"]["MAX_TIME_ROUND"]);
+                MAX_SPAWNS = int.Parse(data["Map"]["MAX_SPAWNS"]);
+                MAX_TIME_LOADING = int.Parse(data["Map"]["MAX_TIME_LOADING"]);
+            }   
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine($"Error {e.StackTrace} Reason: {e.Message}");
+            }
         }
 
         public static void NamesMapRead()
         {
-            string[] fileEntries = Directory.GetFiles(GetPath("spawn_position"));
-            int len = fileEntries.Length;
-            for(int i = 0; i < len; ++i)
-                mapName[i] = Path.GetFileName(fileEntries[i]).Replace(".txt", "");
-            Rand.Shuffle(mapName);
+            try
+            {
+                string[] fileEntries = Directory.GetFiles(GetPath("spawn_position"));
+                int len = fileEntries.Length;
+                for (int i = 0; i < len; ++i)
+                    mapName[i] = Path.GetFileName(fileEntries[i]).Replace(".txt", "");
+                Rand.Shuffle(mapName);
+            }
+            catch(DirectoryNotFoundException e)
+            {
+                Console.WriteLine($"Error {e.StackTrace} Reason: {e.Message}");
+            }
         }
 
         public static Vector3 FlagPositionRead(string section)
@@ -58,36 +79,43 @@ namespace CaptureTheFlag.Map
 
         public static void SpawnPositionRead()
         {
-            string[] lines = File.ReadAllLines(GetPath($"spawn_position{Path.DirectorySeparatorChar}{GetCurrentMap()}.txt"));
-            string[] position;
-            int len = lines.Length - 1;
-            int j = 0;
             try
             {
-                Interior = int.Parse(lines[len]);
-            }
-            catch (FormatException)
-            {
-                Interior = 0;
-                ++len;
-            }
-            Console.WriteLine("  Interior Map: " + Interior);
-            for(int i = 0; i < MAX_SPAWNS; ++i)
-            {
-                position = lines[i].Split(',');
-                spawns[(int)TeamID.Alpha, i].X = Double(position[0]);
-                spawns[(int)TeamID.Alpha, i].Y = Double(position[1]);
-                spawns[(int)TeamID.Alpha, i].Z = Double(position[2]);
-                spawns[(int)TeamID.Alpha, i].Angle = Float(position[3]);
-            }
+                string[] lines = File.ReadAllLines(GetPath($"spawn_position{Path.DirectorySeparatorChar}{GetCurrentMap()}.txt"));
+                string[] position;
+                int len = lines.Length - 1;
+                int j = 0;
+                try
+                {
+                    Interior = int.Parse(lines[len]);
+                }
+                catch (FormatException)
+                {
+                    Interior = 0;
+                    ++len;
+                }
+                Console.WriteLine("  Interior Map: " + Interior);
+                for (int i = 0; i < MAX_SPAWNS; ++i)
+                {
+                    position = lines[i].Split(',');
+                    spawns[(int)TeamID.Alpha, i].X = Double(position[0]);
+                    spawns[(int)TeamID.Alpha, i].Y = Double(position[1]);
+                    spawns[(int)TeamID.Alpha, i].Z = Double(position[2]);
+                    spawns[(int)TeamID.Alpha, i].Angle = Float(position[3]);
+                }
 
-            for (int i = MAX_SPAWNS + 1; i < len; ++i)
+                for (int i = MAX_SPAWNS + 1; i < len; ++i)
+                {
+                    position = lines[i].Split(',');
+                    spawns[(int)TeamID.Beta, j].X = Double(position[0]);
+                    spawns[(int)TeamID.Beta, j].Y = Double(position[1]);
+                    spawns[(int)TeamID.Beta, j].Z = Double(position[2]);
+                    spawns[(int)TeamID.Beta, j++].Angle = Float(position[3]);
+                }
+            }
+            catch(FileNotFoundException e)
             {
-                position = lines[i].Split(',');
-                spawns[(int)TeamID.Beta, j].X = Double(position[0]);
-                spawns[(int)TeamID.Beta, j].Y = Double(position[1]);
-                spawns[(int)TeamID.Beta, j].Z = Double(position[2]);
-                spawns[(int)TeamID.Beta, j++].Angle = Float(position[3]);
+                Console.WriteLine($"Error {e.StackTrace} Reason: {e.Message}");
             }
         }
     }
