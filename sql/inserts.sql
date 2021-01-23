807a53bc8d155aae947ef65629d04694fe16da19
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2021-01-23 12:36:54 -0500

    SQL files created

---
+++ sql/inserts.sql
@@ -0,0 +1,5 @@
+use gamemode;
+
+INSERT INTO Admins (namePlayer, levelAdmin) VALUES ('MrDave1999', 4), ('[DRE]K3NDO_[LT]', 3);
+INSERT INTO Vips (namePlayer, levelVip, skinid) VALUES ('MrDave1999', 3, 146), ('[DRE]K3NDO_[LT]', 2, -1);
+
