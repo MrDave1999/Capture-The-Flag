fb8b3dd62d6f734168eb66cbc6909f552a242261
MrDave1999 <daveseva2010@hotmail.es>
2021-03-03 13:46:56 -0500

    Delete inserts.sql and tables.sql files

--- sql/inserts.sql
+++
@@ -1,5 +0,0 @@
-use gamemode;
-
-INSERT INTO admins (namePlayer, levelAdmin) VALUES ('MrDave1999', 4), ('[DRE]K3NDO_[LT]', 3);
-INSERT INTO vips (namePlayer, levelVip, skinid) VALUES ('MrDave1999', 3, 146), ('[DRE]K3NDO_[LT]', 2, -1);
-
