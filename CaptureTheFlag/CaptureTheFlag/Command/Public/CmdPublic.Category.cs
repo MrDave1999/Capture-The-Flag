2018931d220292b3a6132d8ad819b7f671398250
MrDave1999 <daveseva2010@hotmail.es>
2021-03-30 11:07:59 -0500

    Added the command /vip and /admin

--- CaptureTheFlag/CaptureTheFlag/Command/Public/CmdPublic.Category.cs
+++ CaptureTheFlag/CaptureTheFlag/Command/Public/CmdPublic.Category.cs
@@ -28,8 +28,8 @@ namespace CaptureTheFlag.Command.Public
         public void ShowGeneral()
         {
             DialogCategory.Message = 
-                $"{Color.Yellow}/help - {Color.White} Muestra información sobre como se debe jugar." +
-                $"\n{Color.Yellow}/credits - {Color.White} Muestra las personas que han participado en la creación de la GM." +
+                $"{Color.Yellow}/help{Color.White} - Muestra información sobre como se debe jugar." +
+                $"\n{Color.Yellow}/credits{Color.White} - Muestra las personas que han participado en la creación de la GM." +
                 $"\n{Color.Yellow}/ranks{Color.White} - Muestra la lista de rangos disponibles." +
                 $"\n{Color.Yellow}/rules{Color.White} - Muestra las reglas que se deben cumplir en el servidor." +
                 $"\n{Color.Yellow}/infoadmin{Color.White} - Muestra información sobre como ser admin en el servidor." +
@@ -69,7 +69,7 @@ namespace CaptureTheFlag.Command.Public
         public void ShowStats()
         {
             DialogCategory.Message =
-                $"{Color.Yellow}/tstats - {Color.White} Muestra las estadísticas de ambos equipos (Alpha y Beta)." +
+                $"{Color.Yellow}/tstats{Color.White} - Muestra las estadísticas de ambos equipos (Alpha y Beta)." +
                 $"\n{Color.Yellow}/stats{Color.White} - Muestra las estadísticas de un jugador conectado." +
                 $"\n{Color.Yellow}/statsdb{Color.White} - Muestra las estadísticas de un jugador desconectado." +
                 $"\n{Color.Yellow}/changepass{Color.White} - Cambia la contraseña de la cuenta del jugador." +
@@ -93,21 +93,26 @@ namespace CaptureTheFlag.Command.Public
         public void ShowSigns()
         {
             DialogCategory.Message =
-                 $"{Color.Yellow}Signo (!):{Color.White} Permite hablar en el TeamChat (ejemplo: {Color.Pink}!texto{Color.White})." +
+                $"{Color.Yellow}Signo (!):{Color.White} Permite hablar en el TeamChat (ejemplo: {Color.Pink}!texto{Color.White})." +
                 $"\n{Color.Yellow}Signo (#):{Color.White} Permite hablar en el AdminChat (ejemplo: {Color.Pink}#texto{Color.White})." +
-                $"\n{Color.Yellow}Signo ($):{Color.White} Permite hablar en el VipChat (ejemplo: {Color.Pink}$texto{Color.White})."
+                $"\n{Color.Yellow}Signo ($):{Color.White} Permite hablar en el VipChat (ejemplo: {Color.Pink}$texto{Color.White})." +
+                $"\n{Color.Yellow}Signo (&):{Color.White} Permite hablar como Administrador (ejemplo: {Color.Pink}&texto{Color.White})." +
+                $"\n{Color.Yellow}Signo (@):{Color.White} Permite hablar como usuario VIP (ejemplo: {Color.Pink}@texto{Color.White})." +
+                $"\n{Color.Yellow}Signo (~):{Color.White} Permite hablar como usuario anónimo (ejemplo: {Color.Pink}~texto{Color.White})."
             ;
         }
 
         public void ShowOthers()
         {
             DialogCategory.Message =
-                $"{Color.Yellow}/kill - {Color.White} Asesina al jugador (su vida queda en 0.0)." +
-                $"\n{Color.Yellow}/switch - {Color.White} Permite al jugador cambiarse de equipo." +
-                $"\n{Color.Yellow}/tc - {Color.White} Permite hablar en el Team Chat." +
-                $"\n{Color.Yellow}/re{Color.White} Reinicia a 0 los asesinatos y muertes por ronda." +
+                $"{Color.Yellow}/kill{Color.White} - Asesina al jugador (su vida queda en 0.0)." +
+                $"\n{Color.Yellow}/switch{Color.White} - Permite al jugador cambiarse de equipo." +
+                $"\n{Color.Yellow}/tc{Color.White} - Permite hablar en el Team Chat." +
+                $"\n{Color.Yellow}/re{Color.White} - Reinicia a 0 los asesinatos y muertes por ronda." +
                 $"\n{Color.Yellow}/admins{Color.White} - Muestra la lista de administradores que están conectados." +
+                $"\n{Color.Yellow}/admin{Color.White} - Muestra los niveles administrativos." +
                 $"\n{Color.Yellow}/vips{Color.White} - Muestra los usuarios VIP que están conectados." +
+                $"\n{Color.Yellow}/vip{Color.White} - Muestra los niveles VIP." +
                 $"\n{Color.Yellow}/report{Color.White} - Sirve para reportar a un jugador." +
                 $"\n{Color.Yellow}/music{Color.White} - Permite escuchar música por medio de una URL." +
                 $"\n{Color.Yellow}/stop{Color.White} - Detiene la música." +
