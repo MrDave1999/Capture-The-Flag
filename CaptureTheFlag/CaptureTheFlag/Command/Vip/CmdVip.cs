6aee849bc8e35ad83f0fa3b9b46e094fa6a58d01
MrDave1999 <daveseva2010@hotmail.es>
2021-03-30 10:54:52 -0500

    An explanation was added for each VIP command

--- CaptureTheFlag/CaptureTheFlag/Command/Vip/CmdVip.cs
+++ CaptureTheFlag/CaptureTheFlag/Command/Vip/CmdVip.cs
@@ -11,13 +11,27 @@ namespace CaptureTheFlag.Command.Vip
     [CommandGroup("vip", PermissionChecker = typeof(BlockCommand))]
     public partial class CmdVip
     {
+        [Command("vip", Shortcut = "vip")]
+        private static void UsageMessage(Player player)
+        {
+            player.SendClientMessage(Color.Red, "Usage: /vip [1-3]");
+        }
+
         [Command("1")]
         private static void Cmds_Silver(Player player)
         {
             if (player.IsVipLevel(1)) return;
             new MessageDialog("Comandos - Silver (Nivel 1)",
-                $"{Color.White}/skin, /sskin, /rskin, /saw, /spray, /weather, /vc",
-                "Aceptar", "").Show(player);
+                $"{Color.Yellow}/skin{Color.White} - Permite cambiar de skin." +
+                $"\n{Color.Yellow}/sskin{Color.White} - Guarda el skin de forma automática." +
+                $"\n{Color.Yellow}/rskin{Color.White} - Elimina el skin que hayas guardado." +
+                $"\n{Color.Yellow}/saw{Color.White} - Permite obtener una motosierra." +
+                $"\n{Color.Yellow}/spray{Color.White} - Permite obtener un spray." +
+                $"\n{Color.Yellow}/weather{Color.White} - Permite cambiar de clima." +
+                $"\n{Color.Yellow}/vsay{Color.White} - Te permite hablar como usuario VIP." +
+                $"\n{Color.Yellow}/vc{Color.White} - Permite hablar en el VipChat." +
+                $"\n\n{Color.Orange}También puedes hablar en el VIP Chat con el signo: {Color.White}$texto" +
+                $"\n{Color.Orange}Puedes hablar como usuario VIP con el signo: {Color.White}@texto", "Aceptar", "").Show(player);
         }
 
         [Command("2")]
@@ -25,18 +39,25 @@ namespace CaptureTheFlag.Command.Vip
         {
             if (player.IsVipLevel(2)) return;
             new MessageDialog("Comandos - Gold (Nivel 2)",
-                $"{Color.White}/health, /armour, /jumpon, /jumpoff, /p, /nsay",
-                "Aceptar", "").Show(player);
+                $"{Color.Yellow}/health{Color.White} - Te establece la salud al 100 %" +
+                $"\n{Color.Yellow}/armour{Color.White} - Te establece la armadura al 100 %" +
+                $"\n{Color.Yellow}/jumpon{Color.White} - Activa los saltos mortales." +
+                $"\n{Color.Yellow}/jumpoff{Color.White} - Desactiva los saltos mortales." +
+                $"\n{Color.Yellow}/p{Color.White} - Asigna un paracaidas." +
+                $"\n{Color.Yellow}/nsay{Color.White} - Permite hablar como anónimo." +
+                $"\n\n{Color.Orange}Puedes hablar como usuario anónimo con el signo: {Color.White}~texto", "Aceptar", "").Show(player);
         }
 
         [Command("3")]
         private static void Cmds_Premium(Player player)
         {
-            if (player.IsVipLevel(2)) return;
+            if (player.IsVipLevel(3)) return;
             new MessageDialog("Comandos - Premium (Nivel 3)",
-                $"{Color.White}/invisible, /visible, /flame, /clearchat" +
-                $"\n{Color.Yellow}* Cada vez que inicie una partida tendrás 100 % de adrenalina.",
-                "Aceptar", "").Show(player);
+                $"{Color.Yellow}/invisible{Color.White} - Te permite ser invisible." +
+                $"\n{Color.Yellow}/visible{Color.White} - Quita la invisibilidad." +
+                $"\n{Color.Yellow}/flame{Color.White} - Permite obtener una lanzallamas." +
+                $"\n{Color.Yellow}/clearchat{Color.White} - Borra el chat del jugador." + 
+                $"\n\n{Color.Orange}* Cada vez que inicie una partida tendrás 100 % de adrenalina.", "Aceptar", "").Show(player);
         }
 
         [Command("vips", Shortcut = "vips")]
@@ -53,6 +74,5 @@ namespace CaptureTheFlag.Command.Vip
                 vips.Add(new[] { player1.Name, player1.Data.LevelVip.ToString(), Rank.GetRankVip(player1.Data.LevelVip) });
             vips.Show(player);
         }
-
     }
 }
