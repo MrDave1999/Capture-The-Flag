c615f3a5ef2a5b9c7ede9c76b0a0e8d28cab11a6
MrDave1999 <daveseva2010@hotmail.es>
2021-07-07 22:10:21 -0500

    Create ListDialogEx.cs

---
+++ CaptureTheFlag/CaptureTheFlag/Dialogs/ListDialogEx.cs
@@ -0,0 +1,23 @@
+﻿using SampSharp.GameMode.Display;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaptureTheFlag.Dialogs
+{
+    public class ListDialogEx : ListDialog
+    {
+        public IList<int> Ids { get; } = new List<int>();
+
+        public ListDialogEx(string caption, string button1, string button2 = null) 
+            : base (caption, button1, button2)
+        {
+        }
+
+        public void AddItem(int id, string item)
+        {
+            AddItem(item);
+            Ids.Add(id);
+        }
+    }
+}
