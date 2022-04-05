c8497a61947f593b2c099a40a11d808cd0e3a37a
MrDave1999 <daveseva2010@hotmail.es>
2022-04-05 15:42:07 -0500

    Added support for .NET 6

--- src/Program.cs
+++ src/Program.cs
@@ -1,28 +1,16 @@
 ﻿using CaptureTheFlag.Events;
 using CaptureTheFlag.Utils;
 using SampSharp.Core;
-using System;
-using System.IO;
-using System.Reflection;
 
-namespace CaptureTheFlag
+try
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            try
-            {
-                new GameModeBuilder()
-                    .RedirectConsoleOutput()
-                    .UseEncoding(Scriptfiles.GetPath($"codepages{Path.DirectorySeparatorChar}cp1252.txt"))
-                    .Use<GameMode>()
-                    .Run();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Error {e.StackTrace} Reason: {e.Message}");
-            }
-        }
-    }
+    new GameModeBuilder()
+        .RedirectConsoleOutput()
+        .UseEncoding(Scriptfiles.GetPath($"codepages{Path.DirectorySeparatorChar}cp1252.txt"))
+        .Use<GameMode>()
+        .Run();
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Error {e.StackTrace} Reason: {e.Message}");
 }
