e481a46ea6262ebb3ba42ebe2a1c7dc486773a95
MrDave1999 <daveseva2010@hotmail.es>
2024-08-11 10:01:54 -0500

    test: Add unit tests to account module

---
+++ tests/Application.Tests/Players/Accounts/RoleTests.cs
@@ -0,0 +1,151 @@
+﻿namespace CTF.Application.Tests.Players.Accounts;
+
+public class RoleTests
+{
+    static readonly int[] InvalidRoleCases = [-1, -2, RoleCollection.Max];
+
+    [TestCaseSource(nameof(InvalidRoleCases))]
+    public void SetRole_WhenRoleIdIsInvalid_ShouldReturnsFailureResult(int value)
+    {
+        // Arrange
+        var player = new PlayerInfo();
+        RoleId roleId = (RoleId)value;
+        var expectedMessage = Messages.InvalidRole;
+
+        // Act
+        Result result = player.SetRole(roleId);
+
+        // Asserts
+        result.IsSuccess.Should().BeFalse();
+        result.Message.Should().Be(expectedMessage);
+        player.RoleId.Should().NotBe(roleId);
+    }
+
+    [Test]
+    public void SetRole_WhenRoleIdIsValid_ShouldReturnsSuccessResult()
+    {
+        // Arrange
+        var player = new PlayerInfo();
+        RoleId roleId = RoleId.Admin;
+
+        // Act
+        Result result = player.SetRole(roleId);
+
+        // Asserts
+        result.IsSuccess.Should().BeTrue();
+        player.RoleId.Should().Be(roleId);
+    }
+
+    [Test]
+    public void HasRole_WhenRoleIsAdmin_ShouldReturnsTrue()
+    {
+        // Arrange
+        var player = new PlayerInfo();
+        RoleId roleId = RoleId.Admin;
+        player.SetRole(roleId);
+
+        // Act
+        bool actual = player.HasRole(roleId);
+
+        // Assert
+        actual.Should().BeTrue();
+    }
+
+    [Test]
+    public void HasRole_WhenRoleIsNotAdmin_ShouldReturnsFalse()
+    {
+        // Arrange
+        var player = new PlayerInfo();
+        player.SetRole(RoleId.Admin);
+
+        // Act
+        bool actual = player.HasRole(RoleId.Basic);
+
+        // Assert
+        actual.Should().BeFalse();
+    }
+
+    [Test]
+    public void IsVIP_WhenPlayerIsVIP_ShouldReturnsTrue()
+    {
+        // Arrange
+        var player = new PlayerInfo();
+        player.SetRole(RoleId.VIP);
+
+        // Act
+        bool actual = player.IsVIP();
+
+        // Assert
+        actual.Should().BeTrue();
+    }
+
+    [Test]
+    public void IsVIP_WhenPlayerIsNotVIP_ShouldReturnsFalse()
+    {
+        // Arrange
+        var player = new PlayerInfo();
+        player.SetRole(RoleId.Basic);
+
+        // Act
+        bool actual = player.IsVIP();
+
+        // Assert
+        actual.Should().BeFalse();
+    }
+
+    [Test]
+    public void IsModerator_WhenPlayerIsModerator_ShouldReturnsTrue()
+    {
+        // Arrange
+        var player = new PlayerInfo();
+        player.SetRole(RoleId.Moderator);
+
+        // Act
+        bool actual = player.IsModerator();
+
+        // Assert
+        actual.Should().BeTrue();
+    }
+
+    [Test]
+    public void IsModerator_WhenPlayerIsNotModerator_ShouldReturnsFalse()
+    {
+        // Arrange
+        var player = new PlayerInfo();
+        player.SetRole(RoleId.Basic);
+
+        // Act
+        bool actual = player.IsModerator();
+
+        // Assert
+        actual.Should().BeFalse();
+    }
+
+    [Test]
+    public void IsAdmin_WhenPlayerIsAdmin_ShouldReturnsTrue()
+    {
+        // Arrange
+        var player = new PlayerInfo();
+        player.SetRole(RoleId.Admin);
+
+        // Act
+        bool actual = player.IsAdmin();
+
+        // Assert
+        actual.Should().BeTrue();
+    }
+
+    [Test]
+    public void IsAdmin_WhenPlayerIsNotAdmin_ShouldReturnsFalse()
+    {
+        // Arrange
+        var player = new PlayerInfo();
+        player.SetRole(RoleId.Basic);
+
+        // Act
+        bool actual = player.IsAdmin();
+
+        // Assert
+        actual.Should().BeFalse();
+    }
+}
