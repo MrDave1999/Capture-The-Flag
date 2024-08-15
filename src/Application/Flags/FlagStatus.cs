d04953b70391f9376cff50480d9c652506737d39
MrDave1999 <daveseva2010@hotmail.es>
2024-08-14 21:10:49 -0500

    docs: Add comments to FlagStatus

--- src/Application/Flags/FlagStatus.cs
+++ src/Application/Flags/FlagStatus.cs
@@ -2,10 +2,33 @@
 
 internal enum FlagStatus
 {
+    /// <summary>
+    /// Indicates that the flag is on its base.
+    /// </summary>
     InitialPosition,
+
+    /// <summary>
+    /// Indicates that a player has captured the opposing team's flag from their base.
+    /// </summary>
     Captured,
+
+    /// <summary>
+    /// Indicates that a player has returned the flag to their team's base.
+    /// </summary>
     Returned,
+
+    /// <summary>
+    /// Indicates that a player has taken the flag from a position other than the base.
+    /// </summary>
     Taken,
+
+    /// <summary>
+    /// Indicates that a player has captured the opposing team's flag and brought it back to their own base.
+    /// </summary>
     Brought,
+
+    /// <summary>
+    /// Indicates that a player has dropped the opposing team's flag.
+    /// </summary>
     Dropped
 }
