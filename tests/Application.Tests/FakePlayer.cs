7f8476bf95b7deaa84917b4351779fb9a9882d8e
MrDave1999 <daveseva2010@hotmail.es>
2024-08-19 16:28:02 -0500

    test: Create FakePlayer from the root directory

--- tests/Application.Tests/FakePlayer.cs
+++ tests/Application.Tests/FakePlayer.cs
@@ -1,18 +1,20 @@
-﻿namespace CTF.Application.Tests.Teams;
+﻿namespace CTF.Application.Tests;
 
 public class FakePlayer : Player
 {
     private readonly int _id;
+    private readonly Guid _type;
     public FakePlayer(int id, string name, TeamId team = TeamId.NoTeam)
     {
         _id = id;
+        _type = Guid.NewGuid();
         Name = name;
         Team = (int)team;
     }
 
     public override string Name { get; set; }
     public override int Team { get; set; }
-    public override EntityId Entity => new(Guid.NewGuid(), _id);
+    public override EntityId Entity => new(_type, _id);
     public override bool RemoveAttachedObject(int index) => true;
     public override bool SetAttachedObject(
         int index,
