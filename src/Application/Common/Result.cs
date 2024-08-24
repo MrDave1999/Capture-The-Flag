70d2c03edefbd5245e4ff3ef5ad3aadd6da6e232
MrDave1999 <daveseva2010@hotmail.es>
2024-08-24 16:20:42 -0500

    BREAKING CHANGE: Prevent properties from not being initialized from the outside

--- src/Application/Common/Result.cs
+++ src/Application/Common/Result.cs
@@ -3,32 +3,32 @@
 /// <summary>
 /// Represents the result of an operation that does not return a value.
 /// </summary>
-public readonly ref struct Result
+public ref struct Result
 {
     public Result() { }
 
     /// <summary>
     /// Gets the description of a result.
     /// </summary>
-    public string Message { get; init; } = string.Empty;
+    public string Message { get; private set; } = string.Empty;
 
     /// <summary>
     /// A value indicating that the result was successful.
     /// </summary>
-    public bool IsSuccess { get; init; } = true;
+    public bool IsSuccess { get; private set; } = false;
 
     /// <summary>
     /// A value that indicates that the result was a failure.
     /// </summary>
     public bool IsFailed => !IsSuccess;
 
-    public static Result Success() => new();
-    public static Result Success(string message) => new() { Message = message };
-
-    public static Result Failure() => new() { IsSuccess = false };
-    public static Result Failure(string message) => new()
-    {
-        IsSuccess = false,
-        Message = message
+    public static Result Success() => new() { IsSuccess = true };
+    public static Result Success(string message) => new() 
+    { 
+        IsSuccess = true,
+        Message = message 
     };
+
+    public static Result Failure() => new();
+    public static Result Failure(string message) => new() { Message = message };
 }
