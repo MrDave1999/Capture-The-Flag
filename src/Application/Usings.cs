38e75e376f8a055a5d6b33027708950762fb739d
MrDave1999 <daveseva2010@hotmail.es>
2024-08-29 08:41:33 -0500

    chore: Update global usings

--- src/Application/Usings.cs
+++ src/Application/Usings.cs
@@ -12,6 +12,7 @@ global using Microsoft.Extensions.DependencyInjection;
 
 global using CTF.Application.Common;
 global using CTF.Application.Common.Resources;
+global using CTF.Application.Players.Accounts;
 global using CTF.Application.Players.Ranks;
 global using CTF.Application.Teams;
 global using CTF.Application.Teams.Flags;
