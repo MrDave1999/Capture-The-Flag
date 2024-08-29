741bbb985e5103ee778d597cee0c8cfb6da57f3d
MrDave1999 <daveseva2010@hotmail.es>
2024-08-29 15:22:11 -0500

    feat: Set the value of an object using Reflection

---
+++ src/Application/Common/Extensions/ObjectExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace CTF.Application.Common.Extensions;
+
+public static class ObjectExtensions
+{
+    /// <summary>
+    /// Sets the property value of a specified object.
+    /// </summary>
+    /// <typeparam name="T">The type that represents the object.</typeparam>
+    /// <param name="object">The object whose property value will be set.</param>
+    /// <param name="value">The new property value.</param>
+    /// <param name="propertyName">The property name.</param>
+    /// <exception cref="ArgumentNullException"></exception>
+    /// <exception cref="InvalidOperationException">
+    /// when the property name is not found in the object.
+    /// </exception>
+    public static void SetValue<T>(
+        this T @object,
+        object value,
+        [CallerArgumentExpression(nameof(value))] string propertyName = null) where T : class
+    {
+        ArgumentNullException.ThrowIfNull(@object);
+        ArgumentNullException.ThrowIfNull(value);
+        Type type = @object.GetType();
+        PropertyInfo propertyInfo = type.GetProperty(propertyName) ??
+            throw new InvalidOperationException($"Property '{propertyName}' was not found in type '{type.Name}'.");
+
+        propertyInfo.SetValue(@object, value);
+    }
+}
