dd72444794e2cecde29fac52188387a78194ebd8
MrDave1999 <daveseva2010@hotmail.es>
2024-08-31 10:31:56 -0500

    feat: Add login and signup system

---
+++ src/Application/Players/PlayerSystem.cs
@@ -0,0 +1,17 @@
+﻿namespace CTF.Application.Players;
+
+public class PlayerSystem : ISystem
+{
+    [Event]
+    public bool OnPlayerRequestSpawn(Player player)
+    {
+        var accountComponent = player.GetComponent<AccountComponent>();
+        if (accountComponent is null)
+        {
+            player.SendClientMessage(Color.Red, Messages.LoginOrRegisterToContinue);
+            return false;
+        }
+
+        return true;
+    }
+}
