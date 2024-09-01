779a84e44d7655d044b7da033b6a64a32870d416
MrDave1999 <daveseva2010@hotmail.es>
2024-09-01 11:02:53 -0500

    chore: Update global usings

--- tests/Application.Tests/Usings.cs
+++ tests/Application.Tests/Usings.cs
@@ -7,6 +7,7 @@ global using SampSharp.Entities.SAMP;
 global using CTF.Application.Common;
 global using CTF.Application.Common.Extensions;
 global using CTF.Application.Common.Resources;
+global using CTF.Application.Players;
 global using CTF.Application.Players.Ranks;
 global using CTF.Application.Players.Weapons;
 global using CTF.Application.Players.Accounts;
