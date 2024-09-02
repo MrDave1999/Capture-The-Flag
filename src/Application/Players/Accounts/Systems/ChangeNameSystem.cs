b3120324869cb3238d1c5f4ddfd21887fd8cdbbf
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2024-09-02 16:42:56 -0500

    feat: Allow the player to change their name (#50)

---
+++ src/Application/Players/Accounts/Systems/ChangeNameSystem.cs
@@ -0,0 +1,30 @@
+﻿namespace CTF.Application.Players.Accounts.Systems;
+
+public class ChangeNameSystem(
+    IPlayerRepository playerRepository,
+    IWorldService worldService) : ISystem
+{
+    [PlayerCommand("changename")]
+    public void ChangeName(Player player, string newName)
+    {
+        if(playerRepository.Exists(newName)) 
+        {
+            player.SendClientMessage(Color.Red, Messages.PlayerNameAlreadyExists);
+            return;
+        }
+
+        PlayerInfo playerInfo = player.GetComponent<AccountComponent>().PlayerInfo;
+        string oldName = playerInfo.Name;
+        Result result = playerInfo.SetName(newName);
+        if (result.IsFailed)
+        {
+            player.SendClientMessage(Color.Red, result.Message);
+            return;
+        }
+
+        var message = Smart.Format(Messages.ChangeName, new { OldName = oldName, NewName = newName });
+        worldService.SendClientMessage(Color.Yellow, message);
+        player.Name = newName;
+        playerRepository.UpdateName(playerInfo);
+    }
+}
