a845833ab59f46b129627f48eec4c2243c0d5338
MrDave1999 <daveseva2010@hotmail.es>
2024-09-02 14:32:02 -0500

    feat: Add a command to show player statistics

---
+++ src/Application/Players/Accounts/Systems/PlayerStatsSystem.cs
@@ -0,0 +1,30 @@
+﻿namespace CTF.Application.Players.Accounts.Systems;
+
+public class PlayerStatsSystem(IDialogService dialogService) : ISystem
+{
+    [PlayerCommand("stats")]
+    public void ShowStats(Player player)
+    {
+        PlayerInfo playerInfo = player.GetComponent<AccountComponent>().PlayerInfo;
+        var content =
+        $"""
+        Kills for Round: {playerInfo.StatsPerRound.Kills}
+        Deaths for Round: {playerInfo.StatsPerRound.Deaths}
+        Killing Spree for Round: {playerInfo.StatsPerRound.KillingSpree}
+        Points: {playerInfo.StatsPerRound.Points}/100
+        Max Killing Spree: {playerInfo.MaxKillingSpree}
+        Total Kills: {playerInfo.TotalKills}
+        Total Deaths: {playerInfo.TotalDeaths}
+        Brought Flags: {playerInfo.BroughtFlags}
+        Captured Flags: {playerInfo.CapturedFlags}
+        Dropped Flags: {playerInfo.DroppedFlags}
+        Returned Flags: {playerInfo.ReturnedFlags}
+        HeadShots: {playerInfo.HeadShots}
+        Role: {playerInfo.RoleId}
+        Rank: {playerInfo.RankId}
+        Registration Date: {playerInfo.CreatedAt.GetDateWithStandardFormat()}
+        """;
+        var dialog = new MessageDialog($"Stats: {playerInfo.Name}", content, "Close");
+        dialogService.ShowAsync(player, dialog);
+    }
+}
