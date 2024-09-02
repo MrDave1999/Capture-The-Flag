146a2ee7363dbc0e72b82ce3fe4e4d454ee80d72
MrDave1999 <daveseva2010@hotmail.es>
2024-09-02 14:32:38 -0500

    feat: Create method to get date in standard format

---
+++ src/Application/Common/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace CTF.Application.Common.Extensions;
+
+public static class DateTimeExtensions
+{
+    public static string GetDateWithStandardFormat(this DateTime dateTime)
+        => dateTime.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
+}
