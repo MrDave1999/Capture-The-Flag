eda68edafe541189a57e4554daf53a0810a5e984
MrDave1999 <daveseva2010@hotmail.es>
2024-09-05 15:57:36 -0500

    cleanup: Remove files

--- src/Application/Players/InvalidPlayerExtensions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CTF.Application.Players;
-
-public static class InvalidPlayerExtensions
-{
-    public static bool IsInvalidPlayer(this Player player) => player is null;
-    public static bool IsValidPlayer(this Player player) => player is not null;
-}
