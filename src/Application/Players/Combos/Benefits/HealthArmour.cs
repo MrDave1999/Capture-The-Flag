db563dc450ad85f3516bcbd4136089848e9c2010
MrDave1999 <daveseva2010@hotmail.es>
2024-09-05 18:25:03 -0500

    feat: Introduce flamethrower as a benefit for players

--- src/Application/Players/Combos/Benefits/HealthArmour.cs
+++ src/Application/Players/Combos/Benefits/HealthArmour.cs
@@ -2,14 +2,15 @@
 
 public class HealthArmour : IBenefit
 {
-    public string Name => "150 Health and 100 Armour";
+    public string Name => "100 Health, 100 Armour and FlameThrower";
     public int RequiredPoints => 100;
 
     public void Give(Player player)
     {
         PlayerInfo playerInfo = player.GetInfo();
-        player.Health = 150;
+        player.Health = 100;
         player.Armour = 100;
+        player.GiveWeapon(Weapon.FlameThrower, ammo: 500);
         playerInfo.StatsPerRound.ResetPoints();
     }
 }
