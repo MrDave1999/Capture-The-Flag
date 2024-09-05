efd325b2a84a4f3be8587ddb62d2d625acfc6ffb
MrDave1999 <daveseva2010@hotmail.es>
2024-09-05 16:04:38 -0500

    test: Add unit tests for the extension methods of the Player type

---
+++ tests/Application.Tests/Players/Extensions/HealthArmourExtensionsTests.cs
@@ -0,0 +1,184 @@
+﻿namespace CTF.Application.Tests.Players.Extensions;
+
+public class HealthArmourExtensionsTests
+{
+    [Test]
+    public void AddHealth_WhenAmountIsPositiveAndBelowLimit_ShouldIncreaseHealth()
+    {
+        // Arrange
+        var player = new FakePlayer(id: 1, name: "Bob")
+        {
+            Health = 50
+        };
+        int amount = 20;
+        int expectedHealth = 70;
+
+        // Act
+        player.AddHealth(amount);
+
+        // Assert
+        player.Health.Should().Be(expectedHealth);
+    }
+
+    [Test]
+    public void AddHealth_WhenAmountIsPositiveAndExceedsLimit_ShouldSetHealthToMax()
+    {
+        // Arrange
+        var player = new FakePlayer(id: 1, name: "Bob")
+        {
+            Health = 90
+        };
+        int amount = 20;
+        int expectedHealth = 100;
+
+        // Act
+        player.AddHealth(amount);
+
+        // Assert
+        player.Health.Should().Be(expectedHealth);
+    }
+
+    [Test]
+    public void AddHealth_WhenAmountIsNegative_ShouldConvertToPositiveAndIncreaseHealth()
+    {
+        // Arrange
+        var player = new FakePlayer(id: 1, name: "Bob")
+        {
+            Health = 50
+        };
+        int amount = -20;
+        int expectedHealth = 70;
+
+        // Act
+        player.AddHealth(amount);
+
+        // Assert
+        player.Health.Should().Be(expectedHealth);
+    }
+
+    [Test]
+    public void AddHealth_WhenAmountIsZero_ShouldNotChangeHealth()
+    {
+        // Arrange
+        var player = new FakePlayer(id: 1, name: "Bob")
+        {
+            Health = 50
+        };
+        int amount = 0;
+        int expectedHealth = 50;
+
+        // Act
+        player.AddHealth(amount);
+
+        // Assert
+        player.Health.Should().Be(expectedHealth);
+    }
+
+    [Test]
+    public void AddHealth_WhenPlayerHasMaxHealth_ShouldNotChangeHealth()
+    {
+        // Arrange
+        var player = new FakePlayer(id: 1, name: "Bob")
+        {
+            Health = 100
+        };
+        int amount = 10;
+        int expectedHealth = 100;
+
+        // Act
+        player.AddHealth(amount);
+
+        // Assert
+        player.Health.Should().Be(expectedHealth);
+    }
+
+    [Test]
+    public void AddArmour_WhenAmountIsPositiveAndBelowLimit_ShouldIncreaseArmour()
+    {
+        // Arrange
+        var player = new FakePlayer(id: 1, name: "Bob")
+        {
+            Armour = 50
+        };
+        int amount = 20;
+        int expectedArmour = 70;
+
+        // Act
+        player.AddArmour(amount);
+
+        // Assert
+        player.Armour.Should().Be(expectedArmour);
+    }
+
+    [Test]
+    public void AddArmour_WhenAmountIsPositiveAndExceedsLimit_ShouldSetArmourToMax()
+    {
+        // Arrange
+        var player = new FakePlayer(id: 1, name: "Bob")
+        {
+            Armour = 90
+        };
+        int amount = 20;
+        int expectedArmour = 100;
+
+        // Act
+        player.AddArmour(amount);
+
+        // Assert
+        player.Armour.Should().Be(expectedArmour);
+    }
+
+    [Test]
+    public void AddArmour_WhenAmountIsNegative_ShouldConvertToPositiveAndIncreaseArmour()
+    {
+        // Arrange
+        var player = new FakePlayer(id: 1, name: "Bob")
+        {
+            Armour = 50
+        };
+        int amount = -20;
+        int expectedArmour = 70;
+
+        // Act
+        player.AddArmour(amount);
+
+        // Assert
+        player.Armour.Should().Be(expectedArmour);
+    }
+
+    [Test]
+    public void AddArmour_WhenAmountIsZero_ShouldNotChangeArmour()
+    {
+        // Arrange
+        var player = new FakePlayer(id: 1, name: "Bob")
+        {
+            Armour = 50
+        };
+        int amount = 0;
+        int expectedArmour = 50;
+
+        // Act
+        player.AddArmour(amount);
+
+        // Assert
+        player.Armour.Should().Be(expectedArmour);
+    }
+
+    [Test]
+    public void AddArmour_WhenPlayerHasMaxArmour_ShouldNotChangeArmour()
+    {
+        // Arrange
+        var player = new FakePlayer(id: 1, name: "Bob")
+        {
+            Armour = 100
+        };
+        int amount = 10;
+        int expectedArmour = 100;
+
+        // Act
+        player.AddArmour(amount);
+
+        // Assert
+        player.Armour.Should().Be(expectedArmour);
+    }
+}
---
+++ tests/Application.Tests/Players/Extensions/PlayerExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿namespace CTF.Application.Tests.Players.Extensions;
+
+public class PlayerExtensionsTests
+{
+    [Test]
+    public void GetInfo_WhenNoAccountComponent_ShouldThrowInvalidOperationException()
+    {
+        // Arrange
+        var fakePlayer = new FakePlayer1();
+
+        // Act
+        Action act = () => fakePlayer.GetInfo();
+
+        // Assert
+        act.Should().Throw<InvalidOperationException>();
+    }
+
+    [Test]
+    public void GetInfo_WhenAccountComponentIsAssigned_ShouldNotThrowInvalidOperationException()
+    {
+        // Arrange
+        var fakePlayer = new FakePlayer2();
+
+        // Act
+        Action act = () => fakePlayer.GetInfo();
+
+        // Assert
+        act.Should().NotThrow<InvalidOperationException>();
+    }
+
+    private class FakePlayer1 : Player
+    {
+        public override T GetComponent<T>()
+        {
+            return null;
+        }
+    }
+
+    private class FakePlayer2 : Player
+    {
+        public override T GetComponent<T>()
+        {
+            var accountComponent = new AccountComponent(new PlayerInfo());
+            return accountComponent as T;
+        }
+    }
+}
