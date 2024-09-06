ef19e02203a224317644d877c122f7da6a41a0f7
MrDave1999 <daveseva2010@hotmail.es>
2024-09-06 08:04:58 -0500

    feat!: Add new combos that improve the player's health and armour

---
+++ src/Application/Players/Combos/Services/FlamethrowerVitality.cs
@@ -0,0 +1,16 @@
+﻿namespace CTF.Application.Players.Combos.Services;
+
+public class FlamethrowerVitality : ICombo
+{
+    public string Name => "100 Health, 100 Armour and FlameThrower";
+    public int RequiredPoints => 100;
+
+    public void Give(Player player)
+    {
+        PlayerInfo playerInfo = player.GetInfo();
+        player.Health = 100;
+        player.Armour = 100;
+        player.GiveWeapon(Weapon.FlameThrower, ammo: 500);
+        playerInfo.StatsPerRound.ResetPoints();
+    }
+}
---
+++ src/Application/Players/Combos/Services/SatchelChargesVitality.cs
@@ -0,0 +1,17 @@
+﻿namespace CTF.Application.Players.Combos.Services;
+
+public class SatchelChargesVitality : ICombo
+{
+    public string Name => "100 Health, 100 Armour and 25 Satchel charges";
+    public int RequiredPoints => 100;
+
+    public void Give(Player player)
+    {
+        PlayerInfo playerInfo = player.GetInfo();
+        player.Health = 100;
+        player.Armour = 100;
+        player.GiveWeapon(Weapon.SatchelCharge, ammo: 25);
+        player.GiveWeapon(Weapon.Detonator, ammo: 1);
+        playerInfo.StatsPerRound.ResetPoints();
+    }
+}
