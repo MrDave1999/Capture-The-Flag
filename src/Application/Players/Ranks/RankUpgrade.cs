060ab7726441a9b767b28954c0286c72591cd6b4
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2024-09-07 08:32:14 -0500

    feat: Implement class for upgrading player's rank (#63)

---
+++ src/Application/Players/Ranks/RankUpgrade.cs
@@ -0,0 +1,19 @@
+﻿namespace CTF.Application.Players.Ranks;
+
+public class RankUpgrade(IPlayerRepository playerRepository)
+{
+    public void RankUp(Player player, PlayerInfo playerInfo)
+    {
+        if (playerInfo.CanMoveUpToNextRank())
+        {
+            IRank nextRank = RankCollection.GetNextRank(playerInfo.RankId).Value;
+            player.SendClientMessage(Color.Yellow, Smart.Format(Messages.NextRank, nextRank));
+            player.SendClientMessage(Color.Orange, Messages.RankUpAward);
+            player.Armour = 100;
+            player.Health = 100;
+            playerInfo.StatsPerRound.AddPoints(100);
+            playerInfo.SetRank(nextRank.Id);
+            playerRepository.UpdateRank(playerInfo);
+        }
+    }
+}
