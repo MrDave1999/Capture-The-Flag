aabcd3efd1f2cc00eb20a72cf71c878ffa1f111a
MrDave1999 <daveseva2010@hotmail.es>
2024-09-09 15:33:05 -0500

    feat: Implement player spawn system

--- src/Application/Players/PlayerSystem.cs
+++ src/Application/Players/PlayerSystem.cs
@@ -19,20 +19,6 @@ public class PlayerSystem(
     }
 
     [Event]
-    public bool OnPlayerRequestSpawn(Player player)
-    {
-        var accountComponent = player.GetComponent<AccountComponent>();
-        bool isNotLoggedInOrRegistered = accountComponent is null;
-        if (isNotLoggedInOrRegistered)
-        {
-            player.SendClientMessage(Color.Red, Messages.LoginOrRegisterToContinue);
-            return false;
-        }
-
-        return true;
-    }
-
-    [Event]
     public void OnPlayerDeath(Player deadPlayer, Player killer, Weapon reason)
     {
         worldService.SendDeathMessage(killer, deadPlayer, reason);
