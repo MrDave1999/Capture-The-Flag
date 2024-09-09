9b8947d08138077263f20e5ea7d9f5ca636eb873
MrDave1999 <daveseva2010@hotmail.es>
2024-09-09 16:17:51 -0500

    feat: Implement gamemode init requirements
    * Add classes for class selection
    
    * Load filterscript for the server entry map
    
    * Load the filterscript that represents the map that will be loaded when the server starts
    
    * Create flag pickups when the server starts up

---
+++ src/Host/GameModeInit.cs
@@ -0,0 +1,36 @@
+﻿namespace CTF.Host;
+
+public class GameModeInit(
+    IWorldService worldService,
+    IServerService serverService,
+    MapInfoService mapInfoService,
+    TeamPickupService teamPickupService,
+    TeamIconService teamIconService) : ISystem
+{
+    private readonly CurrentMap _currentMap = mapInfoService.Read();
+
+    [Event]
+    public void OnGameModeInit()
+    {
+        Console.WriteLine("\n----------------------------------");
+        Console.WriteLine(" GameMode Capture The Flag");
+        Console.WriteLine("     Team DeathMatch");
+        Console.WriteLine("----------------------------------\n");
+
+        serverService.SendRconCommand("loadfs EntryMap");
+        serverService.SendRconCommand("loadfs RemoveBuilding");
+        serverService.UsePlayerPedAnims();
+        serverService.DisableInteriorEnterExits();
+        serverService.AddPlayerClass((int)Team.Alpha.SkinId, new Vector3(0, 0, 0), 0);
+        serverService.AddPlayerClass((int)Team.Beta.SkinId, new Vector3(0, 0, 0), 0);
+        serverService.SetGameModeText("Blank game mode");
+        serverService.SendRconCommand($"mapname {_currentMap.Name}");
+        serverService.SendRconCommand($"loadfs {_currentMap.Name}");
+        worldService.SetWeather(_currentMap.Weather);
+        serverService.SetWorldTime(_currentMap.WorldTime);
+        teamPickupService.CreateFlagFromBasePosition(Team.Alpha);
+        teamPickupService.CreateFlagFromBasePosition(Team.Beta);
+        teamIconService.CreateFromBasePosition(Team.Alpha);
+        teamIconService.CreateFromBasePosition(Team.Beta);
+    }
+}
