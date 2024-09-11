30a3e124d33af5f200c5b39510320e26a99f7cf7
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2024-09-10 20:41:29 -0500

    BREAKING CHANGE: Rename method to DisableClassSelection (#75)
    “RemoveFromClassSelection” is used when removing a player from the class selection, but this method doesn't actually do that, instead it just changes the state to false.

--- src/Application/Players/PlayerSpawnSystem.cs
+++ src/Application/Players/PlayerSpawnSystem.cs
@@ -21,7 +21,7 @@ public class PlayerSpawnSystem(MapInfoService mapInfoService) : ISystem
             player.GameText(gameText, 999999999, 3);
             return false;
         }
-        player.RemoveFromClassSelection();
+        player.DisableClassSelection();
         player.GameText("_", 1000, 4);
         accountComponent.PlayerInfo.SetTeam(selectedTeam.Id);
         selectedTeam.Members.Add(player);
