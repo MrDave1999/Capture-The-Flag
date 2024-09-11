5409ed16f63a0324952fbce70612be9461a2fe53
MrDave1999 <daveseva2010@hotmail.es>
2024-09-10 20:01:09 -0500

    Add extension methods to improve Player component's class selection

---
+++ src/Application/Players/Extensions/ClassSelectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace CTF.Application.Players.Extensions;
+
+public static class ClassSelectionExtensions
+{
+    public static bool IsInClassSelection(this Player player)
+        => player.GetComponent<ClassSelectionComponent>().IsInClassSelection;
+
+    public static bool IsNotInClassSelection(this Player player)
+        => !player.IsInClassSelection();
+
+    public static bool HasForcedClassSelection(this Player player)
+        => !player.IsInClassSelection();
+
+    public static void SetInClassSelection(this Player player)
+        => player.GetComponent<ClassSelectionComponent>().IsInClassSelection = true;
+
+    public static void RemoveFromClassSelection(this Player player)
+        => player.GetComponent<ClassSelectionComponent>().IsInClassSelection = false;
+}
