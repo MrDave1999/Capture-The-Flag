c333cfba4470f44cc9500df9c239d901c75ee820
MrDave1999 <daveseva2010@hotmail.es>
2024-09-11 16:22:01 -0500

    docs: Add URL to flag models documentation

--- src/Application/Teams/Flags/FlagModel.cs
+++ src/Application/Teams/Flags/FlagModel.cs
@@ -1,5 +1,8 @@
 ﻿namespace CTF.Application.Teams.Flags;
 
+/// <summary>
+/// See <see href="https://dev.prineside.com/en/gtasa_samp_model_id/search/?q=flag">flag models</see>.
+/// </summary>
 public enum FlagModel
 {
     None = 0,
