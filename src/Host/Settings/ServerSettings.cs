852bd96eaaa00b12e4ffb101951093e583a9c8ed
MrDave1999 <daveseva2010@hotmail.es>
2024-09-12 08:12:25 -0500

    feat: Load SA-MP server info from configuration file

---
+++ src/Host/Settings/ServerSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace CTF.Host.Settings;
+
+public class ServerSettings
+{
+    public string HostName { get; init; } = string.Empty;
+    public string LanguageText { get; init; } = string.Empty;
+    public string GameModeText { get; init; } = string.Empty;
+    public string MapName { get; init; } = string.Empty;
+    public string WebUrl { get; init; } = string.Empty;
+}
