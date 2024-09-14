276b552ecc1c4324cbea2518dc09df8c23b11a75
MrDave1999 <daveseva2010@hotmail.es>
2024-09-14 16:19:10 -0500

    BREAKING CHANGE: Minimize GameModeInit class dependencies
    * The 'GameModeInit' class should not be responsible for adding the classes to the class selection
    
    * The 'GameModeInit' class should not be responsible for setting the default map

--- src/Host/GameModeInit.cs
+++ src/Host/GameModeInit.cs
@@ -1,12 +1,7 @@
 ﻿namespace CTF.Host;
 
 public class GameModeInit(
-    IWorldService worldService,
     IServerService serverService,
-    MapInfoService mapInfoService,
-    MapTextDrawRenderer mapTextDrawRenderer,
-    TeamPickupService teamPickupService,
-    TeamIconService teamIconService,
     ServerSettings serverSettings) : ISystem
 {
     [Event]
@@ -25,24 +20,5 @@ public class GameModeInit(
         serverService.SetGameModeText(serverSettings.GameModeText);
         serverService.UsePlayerPedAnims();
         serverService.DisableInteriorEnterExits();
-        serverService.AddPlayerClass((int)Team.Alpha.SkinId, new Vector3(0, 0, 0), 0);
-        serverService.AddPlayerClass((int)Team.Beta.SkinId, new Vector3(0, 0, 0), 0);
-
-        Result<IMap> mapResult = MapCollection.GetByName(serverSettings.MapName);
-        if (mapResult.IsSuccess)
-        {
-            mapInfoService.Load(mapResult.Value);
-        }
-        CurrentMap currentMap = mapInfoService.Read();
-        serverService.SendRconCommand($"mapname {currentMap.Name}");
-        serverService.SendRconCommand($"loadfs {currentMap.Name}");
-        mapTextDrawRenderer.UpdateMapName();
-
-        worldService.SetWeather(currentMap.Weather);
-        serverService.SetWorldTime(currentMap.WorldTime);
-        teamPickupService.CreateFlagFromBasePosition(Team.Alpha);
-        teamPickupService.CreateFlagFromBasePosition(Team.Beta);
-        teamIconService.CreateFromBasePosition(Team.Alpha);
-        teamIconService.CreateFromBasePosition(Team.Beta);
     }
 }
