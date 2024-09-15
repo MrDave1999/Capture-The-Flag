b43c06d8b0e70f7b35fe0c52f2d675f787793d10
MrDave1999 <daveseva2010@hotmail.es>
2024-09-15 13:50:53 -0500

    feat: Implement a map rotation system

---
+++ src/Application/Maps/Systems/MapRotationSystem.cs
@@ -0,0 +1,18 @@
+﻿namespace CTF.Application.Maps.Systems;
+
+public class MapRotationSystem(
+    MapRotationService mapRotationService,
+    MapTextDrawRenderer mapTextDrawRenderer) : ISystem
+{
+    [Event]
+    public void OnPlayerSpawn(Player player)
+    {
+        mapTextDrawRenderer.Show(player);
+    }
+
+    [Event]
+    public void OnGameModeInit()
+    {
+        mapRotationService.StartRotation();
+    }
+}
