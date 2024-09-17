0ba2a225231aee073fcae91b9f6c1c145ef06ca2
MrDave1999 <daveseva2010@hotmail.es>
2024-09-16 20:44:18 -0500

    feat: Allow moderators to set the time left

--- src/Application/Maps/Systems/MapRotationSystem.cs
+++ src/Application/Maps/Systems/MapRotationSystem.cs
@@ -15,4 +15,22 @@ public class MapRotationSystem(
     {
         mapRotationService.StartRotation();
     }
+
+    [PlayerCommand("settimeleft")]
+    public void SetTimeLeft(Player player, int minutes)
+    {
+        if (player.HasLowerRoleThan(RoleId.Moderator)) 
+            return;
+
+        var interval = new Minutes(minutes);
+        TimeLeft timeLeft = mapRotationService.TimeLeft;
+        Result result = timeLeft.SetInterval(interval);
+        if(result.IsFailed)
+        {
+            player.SendClientMessage(Color.Red, result.Message);
+            return;
+        }
+
+        mapTextDrawRenderer.UpdateTimeLeft(timeLeft);
+    }
 }
