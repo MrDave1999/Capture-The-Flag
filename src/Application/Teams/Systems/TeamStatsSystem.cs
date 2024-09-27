6621b208212c13cb8253c307b59157371c691a9f
MrDave1999 <daveseva2010@hotmail.es>
2024-09-27 13:18:53 -0500

    feat: Allow players to view statistics of both teams (Alpha and Beta)

---
+++ src/Application/Teams/Systems/TeamStatsSystem.cs
@@ -0,0 +1,37 @@
+﻿namespace CTF.Application.Teams.Systems;
+
+public class TeamStatsSystem(
+    IDialogService dialogService, 
+    TeamTextDrawRenderer teamTextDrawRenderer) : ISystem
+{
+    [Event]
+    public void OnPlayerSpawn(Player player)
+    {
+        teamTextDrawRenderer.Show(player);
+    }
+
+    [PlayerCommand("tstats")]
+    public void ShowStats(Player player)
+    {
+        Team alphaTeam = Team.Alpha;
+        Team betaTeam = Team.Beta;
+        var content =
+        $"""
+        {alphaTeam.ColorHex}>>> Alpha Team: 
+        Members: {alphaTeam.Members.Count}
+        Score: {alphaTeam.StatsPerRound.Score}
+        Kills: {alphaTeam.StatsPerRound.Kills}
+        Deaths: {alphaTeam.StatsPerRound.Deaths}
+        Flag captured by: {alphaTeam.Flag.CarrierName}
+
+        {betaTeam.ColorHex}>>> Beta Team: 
+        Members: {betaTeam.Members.Count}
+        Score: {betaTeam.StatsPerRound.Score}
+        Kills: {betaTeam.StatsPerRound.Kills}
+        Deaths: {betaTeam.StatsPerRound.Deaths}
+        Flag captured by: {betaTeam.Flag.CarrierName}
+        """;
+        var dialog = new MessageDialog("Team Stats", content, "Close");
+        dialogService.ShowAsync(player, dialog);
+    }
+}
