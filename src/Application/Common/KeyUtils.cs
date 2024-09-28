f293ce72d2b3f09f0cc40a5bb78f7b21fd2608a1
MrDave1999 <daveseva2010@hotmail.es>
2024-09-28 11:03:36 -0500

    feat: Create class to check for keypresses

---
+++ src/Application/Common/KeyUtils.cs
@@ -0,0 +1,43 @@
+﻿namespace CTF.Application.Common;
+
+/// <summary>
+/// Contains methods to check for keypresses.
+/// </summary>
+public static class KeyUtils
+{
+    /// <summary>
+    /// Checks if <see cref="Keys" /> have been pressed.
+    /// </summary>
+    /// <param name="newKeys">
+    /// New <see cref="Keys" />.
+    /// </param>
+    /// <param name="oldKeys">
+    /// Old <see cref="Keys" />.
+    /// </param>
+    /// <param name="keys">
+    /// The <see cref="Keys" /> to check for.
+    /// </param>
+    /// <returns>
+    /// Whether the <see cref="Keys" /> have been pressed.
+    /// </returns>
+    public static bool HasPressed(Keys newKeys, Keys oldKeys, Keys keys)
+        => newKeys.HasFlag(keys) && !oldKeys.HasFlag(keys);
+
+    /// <summary>
+    /// Checks if <see cref="Keys" /> have been released.
+    /// </summary>
+    /// <param name="newKeys">
+    /// New <see cref="Keys" />.
+    /// </param>
+    /// <param name="oldKeys">
+    /// Old <see cref="Keys" />.
+    /// </param>
+    /// <param name="keys">
+    /// The <see cref="Keys" /> to check for.
+    /// </param>
+    /// <returns>
+    /// Whether the <see cref="Keys" /> have been released.
+    /// </returns>
+    public static bool HasReleased(Keys newKeys, Keys oldKeys, Keys keys)
+        => !newKeys.HasFlag(keys) && oldKeys.HasFlag(keys);
+}
