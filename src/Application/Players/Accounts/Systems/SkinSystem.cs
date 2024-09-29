6d4713054121bad6406e2d83d434570995e09525
MrDave1999 <daveseva2010@hotmail.es>
2024-09-29 18:49:17 -0500

    feat: Allow players to change skins

---
+++ src/Application/Players/Accounts/Systems/SkinSystem.cs
@@ -0,0 +1,22 @@
+﻿namespace CTF.Application.Players.Accounts.Systems;
+
+public class SkinSystem(IPlayerRepository playerRepository) : ISystem
+{
+    [PlayerCommand("skin")]
+    public void SetSkin(Player player, int skinId)
+    {
+        PlayerInfo playerInfo = player.GetInfo();
+        Result result = playerInfo.SetSkin(skinId);
+        if (result.IsFailed)
+        {
+            player.SendClientMessage(Color.Red, result.Message);
+            return;
+        }
+
+        player.Skin = skinId;
+        player.GameText($"Skin ID {skinId}", 3000, 4);
+        playerRepository.UpdateSkin(playerInfo);
+        var message = Smart.Format(Messages.SavedSkin, new { playerInfo.SkinId });
+        player.SendClientMessage(Color.Yellow, message);
+    }
+}
