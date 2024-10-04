cff39af4dd68285bfc18b77fcc513971662a9a2f
MrDave1999 <daveseva2010@hotmail.es>
2024-10-04 13:27:15 -0500

    refactor: Rename parameter to newSkinId for clarity

--- src/Application/Players/Accounts/Systems/ChangeSkinSystem.cs
+++ src/Application/Players/Accounts/Systems/ChangeSkinSystem.cs
@@ -3,18 +3,18 @@
 public class ChangeSkinSystem(IPlayerRepository playerRepository) : ISystem
 {
     [PlayerCommand("skin")]
-    public void SetSkin(Player player, int skinId)
+    public void SetSkin(Player player, [CommandParameter(Name = "skinId")]int newSkinId)
     {
         PlayerInfo playerInfo = player.GetInfo();
-        Result result = playerInfo.SetSkin(skinId);
+        Result result = playerInfo.SetSkin(newSkinId);
         if (result.IsFailed)
         {
             player.SendClientMessage(Color.Red, result.Message);
             return;
         }
 
-        player.Skin = skinId;
-        player.GameText($"Skin ID {skinId}", 3000, 4);
+        player.Skin = newSkinId;
+        player.GameText($"Skin ID {newSkinId}", 3000, 4);
         playerRepository.UpdateSkin(playerInfo);
         var message = Smart.Format(Messages.SavedSkin, new { playerInfo.SkinId });
         player.SendClientMessage(Color.Yellow, message);
