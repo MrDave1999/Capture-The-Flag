87a8ee00679bd3d2938b4703aeef105c0fb54faf
MrDave1999 <daveseva2010@hotmail.es>
2024-10-05 08:11:04 -0500

    feat: Allow administrators to give scores to players

---
+++ src/Application/Players/Accounts/Systems/PlayerScoreSystem.cs
@@ -0,0 +1,101 @@
+﻿namespace CTF.Application.Players.Accounts.Systems;
+
+public class PlayerScoreSystem(
+    IEntityManager entityManager,
+    IWorldService worldService) : ISystem
+{
+    [PlayerCommand("setscore")]
+    public void SetScoreToPlayer(
+        Player currentPlayer, 
+        [CommandParameter(Name = "playerId")]Player targetPlayer,
+        int score)
+    {
+        if (currentPlayer.HasLowerRoleThan(RoleId.Admin))
+            return;
+
+        Result result = targetPlayer.SetScore(score);
+        if (result.IsFailed)
+        {
+            currentPlayer.SendClientMessage(Color.Red, result.Message);
+            return;
+        }
+
+        {
+            var message = Smart.Format(Messages.SetScoreToPlayer, new
+            {
+                PlayerName = targetPlayer.Name,
+                Score = score
+            });
+            currentPlayer.SendClientMessage(Color.Yellow, message);
+        }
+
+        {
+            var message = Smart.Format(Messages.ReceiveScoreFromPlayer, new
+            {
+                PlayerName = currentPlayer.Name,
+                Score = score
+            });
+            targetPlayer.SendClientMessage(Color.Yellow, message);
+        }
+    }
+
+    [PlayerCommand("addscore")]
+    public void AddScoreToPlayer(
+        Player currentPlayer, 
+        [CommandParameter(Name = "playerId")]Player targetPlayer,
+        int score)
+    {
+        if (currentPlayer.HasLowerRoleThan(RoleId.Admin))
+            return;
+
+        Result result = targetPlayer.AddScore(score);
+        if (result.IsFailed)
+        {
+            currentPlayer.SendClientMessage(Color.Red, result.Message);
+            return;
+        }
+
+        {
+            var message = Smart.Format(Messages.AddScoreToPlayer, new
+            {
+                PlayerName = targetPlayer.Name,
+                Score = score
+            });
+            currentPlayer.SendClientMessage(Color.Yellow, message);
+        }
+
+        {
+            var message = Smart.Format(Messages.ReceiveScoreFromPlayer, new
+            {
+                PlayerName = currentPlayer.Name,
+                Score = score
+            });
+            targetPlayer.SendClientMessage(Color.Yellow, message);
+        }
+    }
+
+    [PlayerCommand("addallscore")]
+    public void AddScoreToAllPlayers(Player currentPlayer, int score)
+    {
+        if (currentPlayer.HasLowerRoleThan(RoleId.Admin))
+            return;
+
+        IEnumerable<Player> players = entityManager.GetComponents<Player>();
+        foreach (Player targetPlayer in players)
+        {
+            Result result = targetPlayer.AddScore(score);
+            if (result.IsFailed)
+            {
+                currentPlayer.SendClientMessage(Color.Red, result.Message);
+                return;
+            }
+        }
+
+        var message = Smart.Format(Messages.AddScoreToAllPlayers, new
+        {
+            PlayerName = currentPlayer.Name,
+            Score = score
+        });
+        worldService.SendClientMessage(Color.Yellow, message);
+    }
+}
