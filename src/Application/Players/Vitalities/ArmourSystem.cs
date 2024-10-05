12b931685bbfe5c5e2cbe93c150cb19af6858757
MrDave1999 <daveseva2010@hotmail.es>
2024-10-05 11:54:32 -0500

    feat: Add module to manage player health and armour vitality
    This module allows for dynamic management of players' health and armour, enhancing overall gameplay.

---
+++ src/Application/Players/Vitalities/ArmourSystem.cs
@@ -0,0 +1,69 @@
+﻿namespace CTF.Application.Players.Vitalities;
+
+public class ArmourSystem(
+    IWorldService worldService,
+    IEntityManager entityManager) : ISystem
+{
+    [PlayerCommand("addarmour")]
+    public void AddArmourToPlayer(
+        Player currentPlayer,
+        [CommandParameter(Name = "playerId")]Player targetPlayer,
+        float amount)
+    {
+        if (currentPlayer.HasLowerRoleThan(RoleId.Moderator))
+            return;
+
+        Result<Vitality> result = Vitality.Create(amount);
+        if (result.IsFailed)
+        {
+            currentPlayer.SendClientMessage(Color.Red, result.Message);
+            return;
+        }
+
+        {
+            var message = Smart.Format(Messages.AddArmourToPlayer, new
+            {
+                PlayerName = targetPlayer.Name,
+                Armour = amount
+            });
+            currentPlayer.SendClientMessage(Color.Yellow, message);
+        }
+
+        {
+            var message = Smart.Format(Messages.ReceiveArmourFromPlayer, new
+            {
+                PlayerName = currentPlayer.Name,
+                Armour = amount
+            });
+            targetPlayer.SendClientMessage(Color.Yellow, message);
+            targetPlayer.AddArmour(amount);
+        }
+    }
+
+    [PlayerCommand("addallarmour")]
+    public void AddArmourToAllPlayers(Player currentPlayer, float amount)
+    {
+        if (currentPlayer.HasLowerRoleThan(RoleId.Moderator))
+            return;
+
+        Result<Vitality> result = Vitality.Create(amount);
+        if (result.IsFailed)
+        {
+            currentPlayer.SendClientMessage(Color.Red, result.Message);
+            return;
+        }
+
+        IEnumerable<Player> players = entityManager.GetComponents<Player>();
+        foreach (Player targetPlayer in players)
+        {
+            targetPlayer.AddArmour(amount);
+        }
+
+        var message = Smart.Format(Messages.AddArmourToAllPlayers, new
+        {
+            PlayerName = currentPlayer.Name,
+            Armour = amount
+        });
+        worldService.SendClientMessage(Color.Yellow, message);
+    }
+}
---
+++ src/Application/Players/Vitalities/HealthSystem.cs
@@ -0,0 +1,69 @@
+﻿namespace CTF.Application.Players.Vitalities;
+
+public class HealthSystem(
+    IWorldService worldService,
+    IEntityManager entityManager) : ISystem
+{
+    [PlayerCommand("addhealth")]
+    public void AddHealthToPlayer(
+        Player currentPlayer,
+        [CommandParameter(Name = "playerId")]Player targetPlayer,
+        float amount)
+    {
+        if (currentPlayer.HasLowerRoleThan(RoleId.Moderator))
+            return;
+
+        Result<Vitality> result = Vitality.Create(amount);
+        if(result.IsFailed)
+        {
+            currentPlayer.SendClientMessage(Color.Red, result.Message);
+            return;
+        }
+
+        {
+            var message = Smart.Format(Messages.AddHealthToPlayer, new
+            {
+                PlayerName = targetPlayer.Name,
+                Health = amount
+            });
+            currentPlayer.SendClientMessage(Color.Yellow, message);
+        }
+
+        {
+            var message = Smart.Format(Messages.ReceiveHealthFromPlayer, new
+            {
+                PlayerName = currentPlayer.Name,
+                Health = amount
+            });
+            targetPlayer.SendClientMessage(Color.Yellow, message);
+            targetPlayer.AddHealth(amount);
+        }
+    }
+
+    [PlayerCommand("addallhealth")]
+    public void AddHealthToAllPlayers(Player currentPlayer, float amount)
+    {
+        if (currentPlayer.HasLowerRoleThan(RoleId.Moderator))
+            return;
+
+        Result<Vitality> result = Vitality.Create(amount);
+        if (result.IsFailed)
+        {
+            currentPlayer.SendClientMessage(Color.Red, result.Message);
+            return;
+        }
+
+        IEnumerable<Player> players = entityManager.GetComponents<Player>();
+        foreach (Player targetPlayer in players) 
+        { 
+            targetPlayer.AddHealth(amount);
+        }
+
+        var message = Smart.Format(Messages.AddHealthToAllPlayers, new
+        {
+            PlayerName = currentPlayer.Name,
+            Health = amount
+        });
+        worldService.SendClientMessage(Color.Yellow, message);
+    }
+}
