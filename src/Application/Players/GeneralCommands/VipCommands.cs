7ba590460f3aebaa18e43d78d2c64af24e45af71
MrDave1999 <daveseva2010@hotmail.es>
2024-10-05 19:54:24 -0500

    feat: Add VIP commands

---
+++ src/Application/Players/GeneralCommands/VipCommands.cs
@@ -0,0 +1,31 @@
+﻿namespace CTF.Application.Players.GeneralCommands;
+
+public class VipCommands : ISystem
+{
+    [PlayerCommand("saw")]
+    public void Saw(Player player)
+    {
+        if (player.HasLowerRoleThan(RoleId.VIP))
+            return;
+
+        player.GiveWeapon(Weapon.Chainsaw, 1);
+    }
+
+    [PlayerCommand("spray")]
+    public void Spray(Player player) 
+    {
+        if (player.HasLowerRoleThan(RoleId.VIP))
+            return;
+
+        player.GiveWeapon(Weapon.Spraycan, IWeapon.UnlimitedAmmo);
+    }
+
+    [PlayerCommand("teargas")]
+    public void Teargas(Player player)
+    {
+        if (player.HasLowerRoleThan(RoleId.VIP))
+            return;
+
+        player.GiveWeapon(Weapon.Teargas, IWeapon.UnlimitedAmmo);
+    }
+}
