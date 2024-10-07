a2b46ea3aa5d84a801dd6a1a1321303e9ec236b3
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2024-10-06 20:03:00 -0500

    feat: Add public commands (#143)

---
+++ src/Application/Players/GeneralCommands/PublicCommands.cs
@@ -0,0 +1,81 @@
+﻿namespace CTF.Application.Players.GeneralCommands;
+
+public class PublicCommands(
+    IEntityManager entityManager,
+    IWorldService worldService,
+    IDialogService dialogService,
+    PlayerStatsRenderer playerStatsRenderer) : ISystem
+{
+    [PlayerCommand("kill")]
+    public void Kill(Player player)
+    {
+        PlayerInfo playerInfo = player.GetInfo();
+        if (playerInfo.Team == Team.None)
+        {
+            player.SendClientMessage(Color.Red, Messages.NoTeam);
+            return;
+        }
+        player.Health = 0;
+    }
+
+    [PlayerCommand("re")]
+    public void ResetScore(Player player) 
+    {
+        PlayerInfo playerInfo = player.GetInfo();
+        playerInfo.StatsPerRound.ResetKills();
+        playerInfo.StatsPerRound.ResetDeaths();
+        player.SetScore(0);
+        playerStatsRenderer.UpdateTextDraw(player);
+        var message = Smart.Format(Messages.PlayerScoreReset, new
+        {
+            PlayerName = player.Name,
+            Color = Color.Red
+        });
+        worldService.SendClientMessage(Color.Yellow, message);
+    }
+
+    [PlayerCommand("admins")]
+    public void ShowAdmins(Player currentPlayer)
+    {
+        IEnumerable<PlayerInfo> admins = entityManager
+            .GetComponents<Player>()
+            .Select(player => player.GetInfo())
+            .Where(playerInfo => playerInfo.RoleId >= RoleId.Moderator)
+            .OrderByDescending(playerInfo => playerInfo.RoleId);
+
+        int count = admins.Count();
+        if (count == 0)
+        {
+            currentPlayer.SendClientMessage(Color.Red, Messages.NoMatchFound);
+            return;
+        }
+        var dialog = new ListDialog(caption: $"Admins: {count}", "Close", "");
+        foreach (PlayerInfo playerInfo in admins)
+        {
+            dialog.Add(playerInfo.Name);
+        }
+        dialogService.ShowAsync(currentPlayer, dialog);
+    }
+
+    [PlayerCommand("vips")]
+    public void ShowVIPs(Player currentPlayer)
+    {
+        IEnumerable<PlayerInfo> vips = entityManager
+            .GetComponents<Player>()
+            .Select(player => player.GetInfo())
+            .Where(playerInfo => playerInfo.RoleId >= RoleId.VIP);
+
+        int count = vips.Count();
+        if (count == 0)
+        {
+            currentPlayer.SendClientMessage(Color.Red, Messages.NoMatchFound);
+            return;
+        }
+        var dialog = new ListDialog(caption: $"VIPs: {count}", "Close", "");
+        foreach (PlayerInfo playerInfo in vips)
+        {
+            dialog.Add(playerInfo.Name);
+        }
+        dialogService.ShowAsync(currentPlayer, dialog);
+    }
+}
