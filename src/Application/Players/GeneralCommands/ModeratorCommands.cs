632ee0a833381594a399d7051814c23d5d3476c6
MrDave1999 <daveseva2010@hotmail.es>
2024-10-09 08:30:29 -0500

    feat: Add command to show moderator commands in-game

--- src/Application/Players/GeneralCommands/ModeratorCommands.cs
+++ src/Application/Players/GeneralCommands/ModeratorCommands.cs
@@ -2,6 +2,38 @@
 
 public class ModeratorCommands(IWorldService worldService) : ISystem
 {
+    [PlayerCommand("cmdsmoderator")]
+    public void ShowModeratorCommands(Player player, IDialogService dialogService)
+    {
+        if (player.HasLowerRoleThan(RoleId.Moderator))
+            return;
+
+        var commands =
+        """
+        {Color1}/maps: {Color2}Displays a list of available maps in the game.
+        {Color1}/settimeleft: {Color2}Sets the remaining time for the current game session.
+        {Color1}/startrt: {Color2}Starts the rotation timer for the current map.
+        {Color1}/stoprt: {Color2}Stops the rotation timer for the current map.
+        {Color1}/kick: {Color2}Kicks a player from the game.
+        {Color1}/warn: {Color2}Issues a warning to a player for inappropriate behavior.
+        {Color1}/setspawn: {Color2}Sets a new spawn point for players in the game.
+        {Color1}/addhealth: {Color2}Adds health to a specified player.
+        {Color1}/addallhealth: {Color2}Restores health to all players in the game.
+        {Color1}/addarmour: {Color2}Grants armour to a specified player.
+        {Color1}/addallarmour: {Color2}Gives armour to all players in the game.
+        {Color1}/clearallchat: {Color2}Clears all messages from the chat.
+
+        {Color1}Use the '&' symbol at the start of your message to access the private moderator chat.
+        """;
+        var content = Smart.Format(commands, new
+        {
+            Color1 = Color.Yellow,
+            Color2 = Color.White,
+        });
+        var dialog = new MessageDialog(caption: "Moderator Commands", content, "Close");
+        dialogService.ShowAsync(player, dialog);
+    }
+
     [PlayerCommand("kick")]
     public void Kick(
         Player currentPlayer,
