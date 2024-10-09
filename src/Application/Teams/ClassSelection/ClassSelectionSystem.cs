8224c1e5f758d1fd308781860a0192476735b082
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2024-10-09 16:10:23 -0500

    feat: Add welcome message when player connects to the server (#157)

--- src/Application/Teams/ClassSelection/ClassSelectionSystem.cs
+++ src/Application/Teams/ClassSelection/ClassSelectionSystem.cs
@@ -16,6 +16,8 @@ public class ClassSelectionSystem(
     [Event]
     public void OnPlayerConnect(Player player)
     {
+        player.SendClientMessage(Color.Yellow, Messages.Welcome1);
+        player.SendClientMessage(Color.Red, Messages.Welcome2);
         player.Color = Team.None.ColorHex;
         player.AddComponent<ClassSelectionComponent>();
         player.RemoveAttachedObject(0);
