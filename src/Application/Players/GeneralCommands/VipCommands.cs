f738302f80279017eab8ab55709ae71900a1f46d
MrDave1999 <daveseva2010@hotmail.es>
2024-10-09 08:14:52 -0500

    feat: Add command to show VIP commands in-game

--- src/Application/Players/GeneralCommands/VipCommands.cs
+++ src/Application/Players/GeneralCommands/VipCommands.cs
@@ -2,6 +2,30 @@
 
 public class VipCommands : ISystem
 {
+    [PlayerCommand("cmdsvip")]
+    public void ShowVipCommands(Player player, IDialogService dialogService)
+    {
+        if (player.HasLowerRoleThan(RoleId.VIP))
+            return;
+
+        var commands =
+        """
+        {Color1}/armour: {Color2}Grants you temporary armour, reducing damage taken from attacks.
+        {Color1}/health: {Color2}Restores a portion of your health instantly.
+        {Color1}/saw: {Color2}Deploys a powerful saw to cut through obstacles or defeat enemies.
+        {Color1}/spray: {Color2}Releases a spray that can confuse and distract opponents.
+        {Color1}/teargas: {Color2}Deploys tear gas, impairing visibility and causing disorientation to nearby enemies.
+        {Color1}/givemecoins: {Color2}Awards you with in-game coins, enhancing your resources.
+        """;
+        var content = Smart.Format(commands, new 
+        { 
+            Color1 = Color.Yellow,
+            Color2 = Color.White
+        });
+        var dialog = new MessageDialog(caption: "VIP Commands", content, "Close");
+        dialogService.ShowAsync(player, dialog);
+    }
+
     [PlayerCommand("saw")]
     public void Saw(Player player)
     {
