cbc85893470ce8f730a1208777150ff4650e63dd
MrDave1999 <daveseva2010@hotmail.es>
2024-10-12 07:52:26 -0500

    feat: Add MariaDB support

--- src/Persistence/Persistence.MariaDB/ServiceCollectionExtensions.cs
+++ src/Persistence/Persistence.MariaDB/ServiceCollectionExtensions.cs
@@ -6,7 +6,26 @@ public static class PersistenceMariaDBServicesExtensions
         this IServiceCollection services, 
         IConfiguration configuration)
     {
+        var mariadbSettings = configuration
+            .GetRequiredSection("MariaDB")
+            .Get<MariaDbSettings>();
 
+        var connectionString = new MySqlConnectionStringBuilder()
+        {
+            Server   = mariadbSettings.Server,
+            Port     = mariadbSettings.Port,
+            Database = mariadbSettings.Database,
+            UserID   = mariadbSettings.UserName,
+            Password = mariadbSettings.Password
+        }.ToString();
+
+        mariadbSettings.ConnectionString = connectionString;
+        services.AddSingleton(mariadbSettings);
+        services.AddSingleton<IPlayerRepository, PlayerRepository>();
+
+        var path = Path.Combine("yesql", typeof(PersistenceMariaDBServicesExtensions).Namespace);
+        ISqlCollection sqlCollection = new YeSqlLoader().LoadFromDirectories(path);
+        services.AddSingleton(sqlCollection);
         return services;
     }
 }
