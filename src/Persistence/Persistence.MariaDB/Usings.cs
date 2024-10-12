9aef97c84161de6a206110cad4dea96084b435cf
MrDave1999 <daveseva2010@hotmail.es>
2024-10-12 07:43:07 -0500

    chore: Update global usings

--- src/Persistence/Persistence.MariaDB/Usings.cs
+++ src/Persistence/Persistence.MariaDB/Usings.cs
@@ -1,2 +1,8 @@
 ﻿global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
+global using MySqlConnector;
+global using YeSql.Net;
+global using CTF.Application.Common.Extensions;
+global using CTF.Application.Common.Services;
+global using CTF.Application.Players.Accounts;
+global using CTF.Application.Players.Ranks;
