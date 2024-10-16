bb49ccb27f6c7a54c103e10e0ea5fc8f6f366a1a
MrDave1999 <daveseva2010@hotmail.es>
2024-10-16 10:00:56 -0500

    cleanup: Remove gettime function
    This function returns a signed 32-bit to represent unix time, however in the year 2038 this will stop working and lead to integer overflow.

--- src/Application/Common/NativeObjects/ServerTimeNativeObject.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CTF.Application.Common.NativeObjects;
-
-public class ServerTimeNativeObject : NativeObjectSingleton<ServerTimeNativeObject>
-{
-    #pragma warning disable IDE1006
-    [NativeMethod]
-    public virtual int gettime(out int hour, out int minute, out int second)
-        => throw new NativeNotImplementedException();
-}
