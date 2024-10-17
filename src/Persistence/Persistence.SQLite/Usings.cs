2c5d10b60fa0700d63706e89fb76516bab3aa67e
MrDave1999 <daveseva2010@hotmail.es>
2024-10-17 16:05:22 -0500

    chore: Update global usings

--- src/Persistence/Persistence.SQLite/Usings.cs
+++ src/Persistence/Persistence.SQLite/Usings.cs
@@ -4,6 +4,7 @@ global using Microsoft.Data.Sqlite;
 global using YeSql.Net;
 global using CTF.Application.Common.Extensions;
 global using CTF.Application.Common.Services;
-global using CTF.Application.Players;
 global using CTF.Application.Players.Accounts;
 global using CTF.Application.Players.Ranks;
+global using CTF.Application.Players.TopPlayers;
+global using CTF.Application.Players.TopPlayers.Models;
