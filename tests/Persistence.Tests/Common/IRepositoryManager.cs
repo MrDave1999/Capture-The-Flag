8b317b18af6f8f6230cb2bb0a3b61203f968473b
MrDave1999 <daveseva2010@hotmail.es>
2024-10-17 17:57:52 -0500

    test: Update 'IRepositoryManager' interface

--- tests/Persistence.Tests/Common/IRepositoryManager.cs
+++ tests/Persistence.Tests/Common/IRepositoryManager.cs
@@ -3,5 +3,6 @@
 public interface IRepositoryManager : IDisposable
 {
     IPlayerRepository PlayerRepository { get; }
+    ITopPlayersRepository TopPlayersRepository { get; }
     void InitializeSeedData();
 }
