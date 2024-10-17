c6e66d1c7326b12b4cea3f63b82b3862411ece70
MrDave1999 <daveseva2010@hotmail.es>
2024-10-17 17:53:21 -0500

    chore: update seed data

--- src/Persistence/Persistence.MariaDB/sql/seed_data.sql
+++ src/Persistence/Persistence.MariaDB/sql/seed_data.sql
@@ -23,4 +23,10 @@ VALUES
 ('Admin_Player','$2a$10$60QnEiafBCLfVBMfQkExVeolyBxVHWcSQKTvkxVJj9FUozRpRP/GW',0,0,0,0,0,0,0,0,'Admin',146,0,'2023-10-12 12:19:24','2023-10-13 12:19:24'),
 ('Moderator_Player','$2a$10$60QnEiafBCLfVBMfQkExVeolyBxVHWcSQKTvkxVJj9FUozRpRP/GW',0,0,0,0,0,0,0,0,'Moderator',146,0,'2023-10-12 12:19:24','2023-10-13 12:19:24'),
 ('VIP_Player','$2a$10$60QnEiafBCLfVBMfQkExVeolyBxVHWcSQKTvkxVJj9FUozRpRP/GW',0,0,0,0,0,0,0,0,'VIP',146,0,'2023-10-12 12:19:24','2023-10-13 12:19:24'),
-('Basic_Player','$2a$10$60QnEiafBCLfVBMfQkExVeolyBxVHWcSQKTvkxVJj9FUozRpRP/GW',0,0,0,0,0,0,0,0,'Basic',146,0,'2023-10-12 12:19:24','2023-10-13 12:19:24');
+('Basic_Player','$2a$10$60QnEiafBCLfVBMfQkExVeolyBxVHWcSQKTvkxVJj9FUozRpRP/GW',0,0,0,0,0,0,0,0,'Basic',146,0,'2023-10-12 12:19:24','2023-10-13 12:19:24'),
+('Basic_Player(2)','$2a$10$60QnEiafBCLfVBMfQkExVeolyBxVHWcSQKTvkxVJj9FUozRpRP/GW',150,0,10,0,0,0,0,0,'Basic',131,3,'2023-10-12 12:19:24','2023-10-13 12:19:24'),
+('Basic_Player(3)','$2a$10$60QnEiafBCLfVBMfQkExVeolyBxVHWcSQKTvkxVJj9FUozRpRP/GW',160,0,15,0,0,0,0,0,'Basic',140,3,'2023-10-12 12:19:24','2023-10-13 12:19:24'),
+('Basic_Player(4)','$2a$10$60QnEiafBCLfVBMfQkExVeolyBxVHWcSQKTvkxVJj9FUozRpRP/GW',170,0,20,0,0,0,0,0,'Basic',137,3,'2023-10-12 12:19:24','2023-10-13 12:19:24'),
+('Basic_Player(5)','$2a$10$60QnEiafBCLfVBMfQkExVeolyBxVHWcSQKTvkxVJj9FUozRpRP/GW',200,0,25,0,0,0,0,0,'Basic',100,4,'2023-10-12 12:19:24','2023-10-13 12:19:24'),
+('Basic_Player(6)','$2a$10$60QnEiafBCLfVBMfQkExVeolyBxVHWcSQKTvkxVJj9FUozRpRP/GW',251,0,50,0,0,0,0,0,'Basic',98,5,'2023-10-12 12:19:24','2023-10-13 12:19:24'),
+('Basic_Player(7)','$2a$10$60QnEiafBCLfVBMfQkExVeolyBxVHWcSQKTvkxVJj9FUozRpRP/GW',200,0,30,0,0,0,0,0,'Basic',150,4,'2023-10-12 12:19:24','2023-10-13 12:19:24');
