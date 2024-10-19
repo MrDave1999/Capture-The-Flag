16fb603fe3c04ac3f888fb66df668ee13ede746b
MrDave1999 <daveseva2010@hotmail.es>
2024-10-19 15:58:03 -0500

    fix: Enable the regexp function in SQLite
    This fixes the following error:
    Microsoft.Data.Sqlite.SqliteException : SQLite Error 1: 'unknown function: REGEXP()'.
    
    Refer to the documentation for user-defined functions:
    https://learn.microsoft.com/en-us/dotnet/standard/data/sqlite/user-defined-functions#operators

---
+++ src/Persistence/Persistence.SQLite/Extensions/SqliteConnectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Persistence.SQLite.Extensions;
+
+public static class SqliteConnectionExtensions
+{
+    /// <summary>
+    /// <see href="https://learn.microsoft.com/en-us/dotnet/standard/data/sqlite/user-defined-functions#operators">
+    /// See user-defined functions.
+    /// </see>
+    /// </summary>
+    public static void CreateRegexpFunction(this SqliteConnection connection)
+    {
+        connection.CreateFunction(
+            name: "regexp",
+            function: (string pattern, string input) => Regex.IsMatch(input, pattern)
+        );
+    }
+}
