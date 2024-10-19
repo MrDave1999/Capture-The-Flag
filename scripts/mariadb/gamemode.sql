abe0856e65797e8cfdadc9614051f6a9655e31ce
MrDave1999 <daveseva2010@hotmail.es>
2024-10-19 14:17:47 -0500

    db: Add CHECK constraints on 'players' to ensure data consistency

--- scripts/mariadb/gamemode.sql
+++ scripts/mariadb/gamemode.sql
@@ -1,23 +1,33 @@
-CREATE DATABASE IF NOT EXISTS `gamemode` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
-USE `gamemode`;
+CREATE DATABASE IF NOT EXISTS gamemode;
+USE gamemode;
 
-CREATE TABLE IF NOT EXISTS `players` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
-  `password` varchar(60) NOT NULL,
-  `total_kills` int(11) NOT NULL,
-  `total_deaths` int(11) NOT NULL,
-  `max_killing_spree` int(11) NOT NULL,
-  `brought_flags` int(11) NOT NULL,
-  `captured_flags` int(11) NOT NULL,
-  `dropped_flags` int(11) NOT NULL,
-  `returned_flags` int(11) NOT NULL,
-  `head_shots` int(11) NOT NULL,
-  `role_id` enum('Basic','VIP','Moderator','Admin') NOT NULL,
-  `skin_id` int(11) NOT NULL,
-  `rank_id` int(11) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `last_connection` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `ix_players_name` (`name`) USING BTREE
+CREATE TABLE IF NOT EXISTS players (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(20) NOT NULL
+   CHECK(
+     TRIM(name) != '' AND 
+     LENGTH(name) >= 3 AND 
+     LENGTH(name) <= 20 AND 
+     name REGEXP '^[0-9a-zA-Z\\[\\]\\(\\)\\$\\@\\.=_]+$'
+   ),
+  password varchar(60) NOT NULL 
+  CHECK(
+    TRIM(password) != '') AND
+    LENGTH(password) = 60
+  ),
+  total_kills int(11) NOT NULL CHECK(total_kills >= 0),
+  total_deaths int(11) NOT NULL CHECK(total_deaths >= 0),
+  max_killing_spree int(11) NOT NULL CHECK(max_killing_spree >= 0),
+  brought_flags int(11) NOT NULL CHECK(brought_flags >= 0),
+  captured_flags int(11) NOT NULL CHECK(captured_flags >= 0),
+  dropped_flags int(11) NOT NULL CHECK(dropped_flags >= 0),
+  returned_flags int(11) NOT NULL CHECK(returned_flags >= 0),
+  head_shots int(11) NOT NULL CHECK(head_shots >= 0),
+  role_id enum('Basic','VIP','Moderator','Admin') NOT NULL,
+  skin_id int(11) NOT NULL CHECK(skin_id >= 0 AND skin_id <= 311),
+  rank_id int(11) NOT NULL CHECK(rank_id >= 0 AND rank_id <= 14),
+  created_at datetime NOT NULL,
+  last_connection datetime NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY ix_players_name (name) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
--- scripts/sqlite/gamemode.sql
+++ scripts/sqlite/gamemode.sql
@@ -1,21 +1,30 @@
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "players" (
-  "id" INTEGER,
-  "name" TEXT NOT NULL UNIQUE COLLATE NOCASE,
-  "password" TEXT NOT NULL,
-  "total_kills"	INTEGER NOT NULL,
-  "total_deaths" INTEGER NOT NULL,
-  "max_killing_spree" INTEGER NOT NULL,
-  "brought_flags" INTEGER NOT NULL,
-  "captured_flags" INTEGER NOT NULL,
-  "dropped_flags" INTEGER NOT NULL,
-  "returned_flags" INTEGER NOT NULL,
-  "head_shots" INTEGER NOT NULL,
-  "role_id" INTEGER NOT NULL,
-  "skin_id" INTEGER NOT NULL,
-  "rank_id" INTEGER NOT NULL,
-  "created_at" TEXT NOT NULL,
-  "last_connection" TEXT NOT NULL,
-  PRIMARY KEY("id")
+CREATE TABLE IF NOT EXISTS players (
+  id INTEGER,
+  name TEXT NOT NULL UNIQUE COLLATE NOCASE 
+  CHECK(
+    TRIM(name) != '' AND 
+    LENGTH(name) >= 3 AND 
+    LENGTH(name) <= 20 AND 
+    name REGEXP '^[0-9a-zA-Z\[\]\(\)\$\@._=]+$'
+  ),
+  password TEXT NOT NULL CHECK(
+    TRIM(password) != '') AND
+    LENGTH(password) = 60
+  ),
+  total_kills INTEGER NOT NULL CHECK(total_kills >= 0),
+  total_deaths INTEGER NOT NULL CHECK(total_deaths >= 0),
+  max_killing_spree INTEGER NOT NULL CHECK(max_killing_spree >= 0),
+  brought_flags INTEGER NOT NULL CHECK(brought_flags >= 0),
+  captured_flags INTEGER NOT NULL CHECK(captured_flags >= 0),
+  dropped_flags INTEGER NOT NULL CHECK(dropped_flags >= 0),
+  returned_flags INTEGER NOT NULL CHECK(returned_flags >= 0),
+  head_shots INTEGER NOT NULL CHECK(head_shots >= 0),
+  role_id INTEGER NOT NULL CHECK(role_id >= 0 AND role_id <= 3),
+  skin_id INTEGER NOT NULL CHECK(skin_id >= 0 AND skin_id <= 311),
+  rank_id INTEGER NOT NULL CHECK(rank_id >= 0 AND rank_id <= 14),
+  created_at TEXT NOT NULL,
+  last_connection TEXT NOT NULL,
+  PRIMARY KEY(id)
 ) STRICT;
 COMMIT;
