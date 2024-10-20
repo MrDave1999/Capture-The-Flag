3677f2127dd4308496372d52ce5c1f908cf6a326
MrDave1999 <daveseva2010@hotmail.es>
2024-10-20 09:00:03 -0500

    feat: Allow admins to set total kills to other players

---
+++ src/Application/Players/Accounts/Systems/PlayerKillsSystem.cs
@@ -0,0 +1,55 @@
+﻿namespace CTF.Application.Players.Accounts.Systems;
+
+public class PlayerKillsSystem(
+    IPlayerRepository playerRepository,
+    PlayerStatsRenderer playerStatsRenderer) : ISystem
+{
+    [PlayerCommand("settotalkills")]
+    public void SetTotalKillsToPlayer(
+        Player currentPlayer,
+        [CommandParameter(Name = "playerId")]Player targetPlayer,
+        int kills)
+    {
+        if (currentPlayer.HasLowerRoleThan(RoleId.Admin))
+            return;
+
+        if (targetPlayer.IsUnauthenticated())
+        {
+            currentPlayer.SendClientMessage(Color.Red, Messages.UnauthenticatedPlayer);
+            return;
+        }
+
+        PlayerInfo targetPlayerInfo = targetPlayer.GetInfo();
+        Result result = targetPlayerInfo.SetTotalKills(kills);
+        if (result.IsFailed)
+        {
+            currentPlayer.SendClientMessage(Color.Red, result.Message);
+            return;
+        }
+
+        IRank rank = RankCollection.GetByRequiredKills(kills).Value;
+        if (rank.Id != targetPlayerInfo.RankId)
+        {
+            targetPlayerInfo.SetRank(rank.Id);
+            playerRepository.UpdateRank(targetPlayerInfo);
+            playerStatsRenderer.UpdateTextDraw(targetPlayer);
+        }
+        playerRepository.UpdateTotalKills(targetPlayerInfo);
+        {
+            var message = Smart.Format(Messages.SetKillsToPlayer, new
+            {
+                Kills = kills,
+                PlayerName = targetPlayer.Name
+            });
+            currentPlayer.SendClientMessage(Color.Yellow, message);
+        }
+        {
+            var message = Smart.Format(Messages.ReceiveKillsFromPlayer, new
+            {
+                Kills = kills,
+                PlayerName = currentPlayer.Name
+            });
+            targetPlayer.SendClientMessage(Color.Yellow, message);
+        }
+    }
+}
