55b9900d21d557425d2907b58d09f4f3ce046496
MrDave1999 <daveseva2010@hotmail.es>
2024-10-23 13:47:42 -0500

    cleanup: Remove sampctl from project
    Removed sampctl as it is no longer needed for the current project setup.

--- build-filterscripts.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd filterscripts
-for /d %%D in (*) do (
-	sampctl package ensure --dir "%%~fD"
-	sampctl package build --dir "%%~fD"
-)
--- build-filterscripts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd filterscripts
-for dir in */
-do
-    sampctl package ensure --dir "$PWD/$dir"
-    sampctl package build --dir "$PWD/$dir"
-done
