210604336b7d9387f6b0acca0d8dd8f8302f52cb
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2024-10-26 18:51:33 -0500

    refactor: Add extension method for application settings management (#190)

---
+++ src/Host/Extensions/AppSettingsExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace CTF.Host.Extensions;
+
+public static class AppSettingsExtensions
+{
+    public static IServiceCollection AddSettings(
+        this IServiceCollection services, 
+        IConfiguration configuration)
+    {
+        var serverSettings = configuration
+            .GetRequiredSection("ServerInfo")
+            .Get<ServerSettings>();
+
+        var commandCooldowns = configuration
+            .GetRequiredSection("CommandCooldowns")
+            .Get<CommandCooldowns>();
+
+        var topPlayersSettings = configuration
+            .GetRequiredSection("TopPlayers")
+            .Get<TopPlayersSettings>();
+
+        services
+            .AddSingleton(serverSettings)
+            .AddSingleton(commandCooldowns)
+            .AddSingleton(topPlayersSettings);
+
+        return services;
+    }
+}
