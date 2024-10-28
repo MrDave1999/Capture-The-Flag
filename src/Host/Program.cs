db2014409760a7c90ca8f9bee8f14633667b0952
MrDave1999 <daveseva2010@hotmail.es>
2024-10-28 16:50:17 -0500

    refactor: Load environment variables from 'Program' class

--- src/Host/Program.cs
+++ src/Host/Program.cs
@@ -4,6 +4,11 @@ public class Program
 {
     static void Main(string[] args)
     {
+        new EnvLoader()
+            .EnableFileNotFoundException()
+            .AddEnvFile(".env")
+            .Load();
+
         new GameModeBuilder()
             .UseEcs<Startup>()
             .Run();
