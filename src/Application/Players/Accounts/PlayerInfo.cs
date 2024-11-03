4d6a9cfb0ee637c1bf1ace4d06322594d5af96ec
MrDave1999 <daveseva2010@hotmail.es>
2024-11-03 08:43:01 -0500

    refactor: Use Regex.IsMatch for .NET 6.0
    The reason for this change is that the “GeneratedRegex” attribute is only available as of .NET 7.0.
    See https://learn.microsoft.com/en-us/dotnet/api/system.text.regularexpressions.generatedregexattribute

--- src/Application/Players/Accounts/PlayerInfo.cs
+++ src/Application/Players/Accounts/PlayerInfo.cs
@@ -2,10 +2,13 @@
 
 public partial class PlayerInfo
 {
+    private const string PlayerNamePattern = @"^[0-9a-zA-Z\[\]\(\)\$\@._=]+$";
     private const int NoSkin = -1;
     private const int NoAccount = -1;
-    [GeneratedRegex(@"^[0-9a-zA-Z\[\]\(\)\$\@._=]+$")]
+    #if NET8_0_OR_GREATER
+    [GeneratedRegex(PlayerNamePattern)]
     private static partial Regex PlayerNameRegex();
+    #endif
 
     public PlayerInfo() { }
 
@@ -89,8 +92,13 @@ public partial class PlayerInfo
         if (value.Length < 3 || value.Length > 20)
             return Result.Failure(Messages.PlayerNameLength);
 
+        #if NET6_0
+        if (!Regex.IsMatch(value, PlayerNamePattern))
+            return Result.Failure(Messages.InvalidNickName);
+        #else
         if (!PlayerNameRegex().IsMatch(value))
             return Result.Failure(Messages.InvalidNickName);
+        #endif
 
         Name = value;
         return Result.Success();
