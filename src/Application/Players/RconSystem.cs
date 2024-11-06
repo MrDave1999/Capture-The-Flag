516a851492c4741c55a75f68e63659dfaf44c7d8
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2024-11-05 21:20:45 -0500

    feat: Prevent player from logging in as RCON, even with correct password (#213)
    This change is to improve security. There is no need to use RCON commands, for that an admin system was implemented.

---
+++ src/Application/Players/RconSystem.cs
@@ -0,0 +1,31 @@
+﻿namespace CTF.Application.Players;
+
+public class RconSystem(IEntityManager entityManager) : ISystem
+{
+    /// <summary>
+    /// This callback is called when someone attempts to log in to RCON in-game, 
+    /// regardless of whether this attempt is successful or not.
+    /// </summary>
+    /// <param name="ip">
+    /// The IP address of the player who attempted to log in to RCON.
+    /// </param>
+    /// <param name="password">
+    /// The password used in the login attempt.
+    /// </param>
+    /// <param name="success">
+    /// false if the password was incorrect, or true if it was correct.
+    /// </param>
+    [Event]
+    public void OnRconLoginAttempt(string ip, string password, bool success)
+    {
+        var players = entityManager.GetComponents<Player>();
+        foreach (Player player in players)
+        {
+            if (player.Ip == ip)
+            {
+                player.Kick();
+                break;
+            }
+        }
+    }
+}
