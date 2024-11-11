12e4e0e4eae99da361a922da92fdb51351a40f37
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2024-11-11 11:26:04 -0500

    refactor: Delegate login dialog view responsibility to LoginDialogViewer (#221)

--- src/Application/Players/ServiceCollectionExtensions.cs
+++ src/Application/Players/ServiceCollectionExtensions.cs
@@ -8,6 +8,7 @@ public static class PlayerServicesExtensions
             .AddSingleton<PlayerRankUpdater>()
             .AddSingleton<KillingSpreeUpdater>()
             .AddSingleton<PlayerStatsRenderer>()
+            .AddSingleton<LoginDialogViewer>()
             .AddComboServices()
             .AddChatServices();
 
