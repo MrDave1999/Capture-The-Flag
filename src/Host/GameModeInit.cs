3d69e29296bb13b8872d68e250a860f25e71e009
MrDave1999 <daveseva2010@hotmail.es>
2024-12-09 17:58:43 -0500

    fix: Update RCON command to use 'website' key instead of 'weburl'
    This change fixes this warning generated by the open.mp server:
    * Legacy key weburl supplied, using website

--- src/Host/GameModeInit.cs
+++ src/Host/GameModeInit.cs
@@ -16,7 +16,7 @@ public class GameModeInit(
         serverService.SendRconCommand("loadfs RemoveBuilding");
         serverService.SendRconCommand($"name {serverSettings.HostName}");
         serverService.SendRconCommand($"language {serverSettings.LanguageText}");
-        serverService.SendRconCommand($"weburl {serverSettings.WebUrl}");
+        serverService.SendRconCommand($"website {serverSettings.WebUrl}");
         serverService.SetGameModeText(serverSettings.GameModeText);
         serverService.UsePlayerPedAnims();
         serverService.DisableInteriorEnterExits();
