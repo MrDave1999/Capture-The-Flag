8a26b3bfaaced9a9fc08e1f09747c13f1ebb6994
MrDave1999 <daveseva2010@hotmail.es>
2025-01-05 18:52:40 -0500

    feat: add COLT45 to the list of available weapons

--- src/Application/Players/Weapons/GtaWeapons.cs
+++ src/Application/Players/Weapons/GtaWeapons.cs
@@ -5,6 +5,7 @@ public class GtaWeapons
     private static readonly GtaWeapon[] s_weapons =
     [
         new(Weapon.Knife,           1),
+        new(Weapon.Colt45,          2),
         new(Weapon.Silenced,        2),
         new(Weapon.Deagle,          2),
         new(Weapon.Shotgun,         3),
