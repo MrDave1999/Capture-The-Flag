a4f4b6a562d8a850e46d786ac157f2f179676f4a
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2025-01-08 08:43:26 -0500

    refactor!: change return type of Give method from void to Result (#279)
    This change enables caller code to perform explicit error handling.

--- src/Application/Players/Combos/Services/GrenadesVitality.cs
+++ src/Application/Players/Combos/Services/GrenadesVitality.cs
@@ -5,12 +5,13 @@ public class GrenadesVitality : ICombo
     public string Name => "100 Health, 100 Armour and Grenades";
     public int RequiredCoins => 100;
 
-    public void Give(Player player)
+    public Result Give(Player player)
     {
         PlayerInfo playerInfo = player.GetInfo();
         player.Health = 100;
         player.Armour = 100;
         player.GiveWeapon(Weapon.Grenade, ammo: 6);
         playerInfo.StatsPerRound.ResetCoins();
+        return Result.Success();
     }
 }
