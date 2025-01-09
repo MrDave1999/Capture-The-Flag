eb41c39c6c5c3fa272eafc3edad232f7f398fc84
MrDave1999 <daveseva2010@hotmail.es>
2025-01-09 09:57:45 -0500

    feat: allow moderators to reset the flag to its base position
    Added functionality for moderators to reset the flag if it's lost within the map or if the flag carrier moves outside the map boundaries.

---
+++ src/Application/Teams/Flags/Systems/ResetFlagSystem.cs
@@ -0,0 +1,49 @@
+﻿namespace CTF.Application.Teams.Flags.Systems;
+
+public class ResetFlagSystem(
+    IWorldService worldService,
+    TeamPickupService teamPickupService,
+    TeamSoundsService teamSoundsService,
+    FlagAutoReturnTimer flagAutoReturnTimer) : ISystem
+{
+    [PlayerCommand("resetflag")]
+    public void ResetToBasePosition(
+        Player player, 
+        [CommandParameter(Name = "red/blue")]string color)
+    {
+        if (player.HasLowerRoleThan(RoleId.Moderator))
+            return;
+
+        Team team = color.ToLower() switch
+        {
+            "red" => Team.Alpha,
+            "blue" => Team.Beta,
+            _ => null
+        };
+
+        if (team is null)
+        {
+            player.SendClientMessage(Color.Red, Messages.InvalidFlagColor);
+            return;
+        }
+
+        ResetFlagPosition(player, team);
+    }
+
+    private void ResetFlagPosition(Player player, Team team)
+    {
+        var message = Smart.Format(Messages.ResetFlagPosition, new
+        {
+            PlayerName = player.Name,
+            team.ColorName
+        });
+        team.IsFlagAtBasePosition = true;
+        team.Flag.RemoveCarrier();
+        teamPickupService.CreateFlagFromBasePosition(team);
+        teamPickupService.DestroyExteriorMarker(team);
+        teamSoundsService.PlayFlagReturnedSound(team);
+        flagAutoReturnTimer.Stop(team);
+        worldService.GameText($"~n~~n~~n~{team.GameText}{team.ColorName} flag returned!", 5000, 3);
+        worldService.SendClientMessage(Color.Yellow, message);
+    }
+}
