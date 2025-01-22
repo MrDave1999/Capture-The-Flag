507a2b2435afabcfca0c4f85b7c117ab7b0d2c9d
MrDave1999 <daveseva2010@hotmail.es>
2025-01-22 13:59:02 -0500

    feat: allow admins to disable anti-C-Bug via commands

---
+++ src/Application/Players/AntiCBug/AntiCBugCommands.cs
@@ -0,0 +1,34 @@
+﻿namespace CTF.Application.Players.AntiCBug;
+
+public class AntiCBugCommands(
+    IWorldService worldService,
+    AntiCBugSettings antiCBugSettings) : ISystem
+{
+    [PlayerCommand("anticbugoff")]
+    public void Disable(Player player)
+    {
+        if (player.HasLowerRoleThan(RoleId.Admin))
+            return;
+
+        var message = Smart.Format(Messages.DisableAntiCBug, new
+        {
+            PlayerName = player.Name
+        });
+        worldService.SendClientMessage(Color.Yellow, message);
+        antiCBugSettings.Disabled = true;
+    }
+
+    [PlayerCommand("anticbugon")]
+    public void Enable(Player player)
+    {
+        if (player.HasLowerRoleThan(RoleId.Admin))
+            return;
+
+        var message = Smart.Format(Messages.EnableAntiCBug, new
+        {
+            PlayerName = player.Name
+        });
+        worldService.SendClientMessage(Color.Yellow, message);
+        antiCBugSettings.Disabled = false;
+    }
+}
