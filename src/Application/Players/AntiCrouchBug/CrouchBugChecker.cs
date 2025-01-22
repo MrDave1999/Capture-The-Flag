134453bee61b2005f9477ccac712e7db31044bbc
MrDave1999 <daveseva2010@hotmail.es>
2025-01-22 11:43:39 -0500

    feat: implement anti-C-Bug mechanism for Desert Eagle

---
+++ src/Application/Players/AntiCrouchBug/CrouchBugChecker.cs
@@ -0,0 +1,48 @@
+﻿namespace CTF.Application.Players.AntiCrouchBug;
+
+public class CrouchBugChecker(UnixTimeSeconds unixTimeSeconds) : ISystem
+{
+    [Event]
+    public void OnPlayerConnect(Player player)
+    {
+        player.AddComponent<LastFiredTimeComponent>();
+    }
+
+    [Event]
+    public void OnPlayerKeyStateChange(Player player, Keys newKeys, Keys oldKeys)
+    {
+        if (player.State != PlayerState.OnFoot)
+            return;
+
+        var lastFiredTimeComponent = player.GetComponent<LastFiredTimeComponent>();
+        if (player.SpecialAction != SpecialAction.Duck && 
+            KeyUtils.HasPressed(newKeys, oldKeys, Keys.Fire))
+        {
+            lastFiredTimeComponent.Value = player.Weapon switch
+            {
+                Weapon.Deagle => unixTimeSeconds.Value,
+                _ => default
+            };
+        }
+        else if (KeyUtils.HasPressed(newKeys, oldKeys, Keys.Crouch))
+        {
+            long currentTime = unixTimeSeconds.Value;
+            long elapsedTime = currentTime - lastFiredTimeComponent.Value;
+            if (elapsedTime < 1)
+            {
+                player.GameText("~r~~h~DON'T C-BUG!", 3000, 4);
+                player.ApplyAnimation(
+                    animationLibrary: "PED", 
+                    animationName: "getup", 
+                    fDelta: 4.1f, 
+                    loop: false, 
+                    lockX: false, 
+                    lockY: false, 
+                    freeze: false, 
+                    time: 0, 
+                    forceSync: false
+                );
+            }
+        }
+    }
+}
---
+++ src/Application/Players/AntiCrouchBug/LastFiredTimeComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace CTF.Application.Players.AntiCrouchBug;
+
+/// <summary>
+/// Represents a component that stores the last shot time and 
+/// is used to detect rapid shooting techniques such as C-Bug.
+/// </summary>
+public class LastFiredTimeComponent : Component
+{
+    public long Value { get; set; }
+}
